4ae175a12952d5be740b4cee3cfa42c7dbec70b1
semyon422 <semyon422@gmail.com>
2019-02-16 16:05:56 +0000

    Package module improved

--- aqua/package/init.lua
+++ aqua/package/init.lua
@@ -3,11 +3,16 @@ local path = package.path
 
 local package = {}
 
+local paths = {}
+
 package.reset = function()
 	_package.path = path .. ";./?/init.lua;./?/?.lua"
+	paths = {}
 end
 
 package.add = function(path)
+	if paths[path] then return end
+	
 	_package.path = _package.path .. (";./path/?.lua;./path/?/init.lua;./path/?/?.lua"):gsub("path", path)
 end
 
