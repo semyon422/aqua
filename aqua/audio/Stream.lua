4b9970a2eb506e22e3c1807ed62cba5d33611d0d
semyon422 <semyon422@users.noreply.github.com>
2019-06-15 19:40:06 +0500

    Add Stream.free method

--- aqua/audio/Stream.lua
+++ aqua/audio/Stream.lua
@@ -1,3 +1,4 @@
+local ffi = require("ffi")
 local bass = require("aqua.audio.bass")
 local bass_fx = require("aqua.audio.bass_fx")
 local Audio = require("aqua.audio.Audio")
@@ -6,6 +7,10 @@ local Stream = Audio:new()
 
 Stream.construct = function(self)
 	self.channel = bass.BASS_StreamCreateFile(false, self.path, 0, 0, 0)
+end
+
+Stream.free = function(self)
+	bass.BASS_StreamFree(self.channel)
 end
 
 return Stream
