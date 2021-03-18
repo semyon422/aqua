cfbf9f71cb9d7688ca29561c264f024b81ae21c8
semyon422 <semyon422@users.noreply.github.com>
2021-03-18 12:07:41 +0500

    Use love.filesystem.setRequirePath in aqua.package

--- aqua/package/init.lua
+++ aqua/package/init.lua
@@ -1,23 +1,20 @@
-local _package = package
-local path = package.path
-local cpath = package.cpath
+local requirePath = love.filesystem.getRequirePath()
+local cRequirePath = love.filesystem.getCRequirePath()
 
 local package = {}
 
-local paths = {}
-
 package.reset = function()
-	_package.path = path .. ";./?/init.lua;./?/?.lua"
-	_package.cpath = cpath
-	paths = {}
+	love.filesystem.setRequirePath(requirePath)
+	love.filesystem.setCRequirePath(cRequirePath)
 end
 
 package.add = function(path)
-	if paths[path] then return end
+	local requirePath = love.filesystem.getRequirePath()
+    love.filesystem.setRequirePath(requirePath .. (";path/?.lua;path/?/init.lua"):gsub("path", path))
 
-	_package.path = _package.path .. (";./path/?.lua;./path/?/init.lua;./path/?/?.lua"):gsub("path", path)
-	_package.cpath = _package.cpath .. (";./path/?.dll;./path/?/?.dll"):gsub("path", path)
-	_package.cpath = _package.cpath .. (";./path/?.so;./path/?/?.so"):gsub("path", path)
+	local cRequirePath = love.filesystem.getCRequirePath()
+    love.filesystem.setCRequirePath(cRequirePath .. (";path/?.dll"):gsub("path", path))
+    love.filesystem.setCRequirePath(cRequirePath .. (";path/?.so"):gsub("path", path))
 end
 
 package.reset()
