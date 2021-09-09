e00d6a9bbc56670eaa3df221be4a290868895b32
semyon422 <semyon422@users.noreply.github.com>
2021-09-09 19:23:04 +0500

    Rename fields to key

--- aqua/util/inside.lua
+++ aqua/util/inside.lua
@@ -1,18 +1,18 @@
-return function(t, fields)
+return function(t, key)
 	local value = t
-	if type(fields) == "table" then
-		for _, key in ipairs(fields) do
+	if type(key) == "table" then
+		for _, key in ipairs(key) do
 			if type(value) ~= "table" then
 				return
 			end
 			value = value[key]
 		end
-	elseif type(fields) == "string" then
-		for key in fields:gmatch("[^.]+") do
+	elseif type(key) == "string" then
+		for subkey in key:gmatch("[^.]+") do
 			if type(value) ~= "table" then
 				return
 			end
-			value = value[key]
+			value = value[subkey]
 		end
 	else
 		return
