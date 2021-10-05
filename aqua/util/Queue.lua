26cb56807bf2d3d2b7887c22cef3b9ba7e04df85
semyon422 <semyon422@users.noreply.github.com>
2021-10-05 22:30:04 +0500

    Add event queue class

---
+++ aqua/util/Queue.lua
@@ -0,0 +1,36 @@
+local Queue = {}
+
+Queue.new = function()
+	local self = {}
+
+	self.events = {}
+	self.count = 0
+
+	return setmetatable(self, {
+		__index = Queue,
+		__call = coroutine.wrap(function()
+			while true do
+				while self.count > 0 do
+					coroutine.yield(self:remove())
+				end
+				coroutine.yield()
+			end
+		end)
+	})
+end
+
+Queue.add = function(self, event)
+	self.count = self.count + 1
+	self.events[self.count] = event
+end
+
+Queue.remove = function(self)
+	local events = self.events
+	local count = self.count
+	local event = events[count]
+	events[count] = nil
+	self.count = count - 1
+	return event
+end
+
+return Queue
