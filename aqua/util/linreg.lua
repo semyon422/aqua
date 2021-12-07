b7aae2d6869444f181c8883a4458729ec3812bfa
semyon422 <semyon422@users.noreply.github.com>
2021-12-07 10:47:52 +0300

    Add linear regression function

---
+++ aqua/util/linreg.lua
@@ -0,0 +1,35 @@
+local function avg(x)
+	local s = 0
+	for i = 1, #x do
+		s = s + x[i]
+	end
+	return s / #x
+end
+
+local function avg2(x)
+	local s = 0
+	for i = 1, #x do
+		s = s + x[i] ^ 2
+	end
+	return s / #x
+end
+
+local function avgp(x, y)
+	local s = 0
+	for i = 1, #x do
+		s = s + x[i] * y[i]
+	end
+	return s / #x
+end
+
+return function(x, y)
+	local sxy = avgp(x, y)
+	local sx = avg(x)
+	local sy = avg(y)
+	local sx2 = avg2(x)
+
+	local b = (sxy - sx * sy) / (sx2 - sx ^ 2)
+	local a = sy - b * sx
+
+	return a, b  -- f(x) = a + b * x
+end
