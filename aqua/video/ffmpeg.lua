848950e0bbefdcbd85a6c2c57a0e63e63d2e9b29
semyon422 <semyon422@users.noreply.github.com>
2022-02-22 21:00:26 +0500

    Fix crash in new ffmpeg versions, video may not work

--- aqua/video/ffmpeg.lua
+++ aqua/video/ffmpeg.lua
@@ -1696,7 +1696,4 @@ ffmpeg.avformat = ffi.load(dl.get("avformat"))
 ffmpeg.avutil = ffi.load(dl.get("avutil"))
 ffmpeg.swscale = ffi.load(dl.get("swscale"))
 
-ffmpeg.avformat.av_register_all()
-ffmpeg.avcodec.avcodec_register_all()
-
 return ffmpeg
