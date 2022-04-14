c351ce11df4b331f0333891443cedbe7a25a593b
semyon422 <semyon422@users.noreply.github.com>
2022-04-14 22:03:54 +0500

    Modify package.path too

--- aqua/package.lua
+++ aqua/package.lua
@@ -1,17 +1,24 @@
 local requirePath = love.filesystem.getRequirePath()
 local cRequirePath = love.filesystem.getCRequirePath()
+local requirePathLua = package.path
+local cRequirePathLua = package.cpath
 
+local _package = package
 local package = {}
 
 package.reset = function()
 	love.filesystem.setRequirePath(requirePath)
 	love.filesystem.setCRequirePath(cRequirePath)
+	package.path = requirePathLua
+	package.cpath = cRequirePathLua
 end
 
 local ext = jit.os == "Windows" and "dll" or "so"
 package.add = function(path)
-    love.filesystem.setRequirePath(love.filesystem.getRequirePath() .. (";path/?.lua;path/?/init.lua"):gsub("path", path))
-    love.filesystem.setCRequirePath(love.filesystem.getCRequirePath() .. (";path/?." .. ext):gsub("path", path))
+	love.filesystem.setRequirePath(love.filesystem.getRequirePath() .. (";path/?.lua;path/?/init.lua"):gsub("path", path))
+	love.filesystem.setCRequirePath(love.filesystem.getCRequirePath() .. (";path/?." .. ext):gsub("path", path))
+	_package.path = _package.path .. (";path/?.lua;path/?/init.lua"):gsub("path", path)
+	_package.cpath = _package.cpath .. (";path/?." .. ext):gsub("path", path)
 end
 
 return package
