3f13d3cabb0aeb185462dea5c0b244338e836de7
semyon422 <semyon422@users.noreply.github.com>
2022-04-22 15:41:32 +0500

    Add aqua.imgui.Keymap class

--- aqua/imgui/Devices.lua
+++ aqua/imgui/Devices.lua
@@ -1,5 +1,5 @@
 local Devices = {
-	"unknown",
+	[0] = "unknown",
 	"keyboard",
 	"gamepad",
 	"midi",
---
+++ aqua/imgui/Keymap.lua
@@ -0,0 +1,46 @@
+local imgui = require("cimgui")
+local Devices = require("aqua.imgui.Devices")
+
+local keymap = imgui.love.keymap
+local gamepad_map = imgui.love.gamepad_map
+
+local Keymap = {}
+
+function Keymap:get(keyPtr, devicePtr)
+	local device = Devices[devicePtr[0]] or "unknown"
+	local key = "unknown"
+	if device == "keyboard" then
+		key = keymap[keyPtr[0]]
+	elseif device == "gamepad" then
+		key = gamepad_map[keyPtr[0]]
+	elseif device == "midi" then
+		key = tostring(keyPtr[0])
+	end
+	return key, device
+end
+
+function Keymap:set(keyPtr, devicePtr, key, device)
+	if device == "keyboard" then
+		local k = keymap[key]
+		if type(k) == "table" then
+			k = k[1]
+		end
+		keyPtr[0] = k or 0
+		devicePtr[0] = Devices.keyboard
+	elseif device == "gamepad" then
+		local k = gamepad_map[key]
+		if type(k) == "table" then
+			k = k[1]
+		end
+		keyPtr[0] = k or 0
+		devicePtr[0] = Devices.gamepad
+	elseif device == "midi" then
+		keyPtr[0] = key or 0
+		devicePtr[0] = Devices.midi
+	else
+		keyPtr[0] = 0
+		devicePtr[0] = 0
+	end
+end
+
+return Keymap
