74180181dd2430c67dd453da79dedcec9a7748e9
semyon422 <semyon422@users.noreply.github.com>
2022-05-06 19:03:33 +0500

    No need to call BASS_PluginLoad

--- aqua/audio/bass_fx.lua
+++ aqua/audio/bass_fx.lua
@@ -6,8 +6,4 @@ HSTREAM BASS_FX_TempoCreate(DWORD chan, DWORD flags);
 HSTREAM BASS_FX_ReverseCreate(DWORD chan, float dec_block, DWORD flags);
 ]])
 
-local bass = require("aqua.audio.bass")
-
-bass.BASS_PluginLoad(dl.get("bass_fx"), 0)
-
 return setmetatable({}, {__index = ffi.load(dl.get("bass_fx"), true)})
