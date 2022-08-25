9c6bd3ed9e0695d3fcdec8b9da7190f7caeb0122
semyon422 <semyon422@users.noreply.github.com>
2022-08-25 18:20:59 +0500

    Add default value and fix indentation

--- aqua/utf8.lua
+++ aqua/utf8.lua
@@ -4,19 +4,20 @@ local utf8 = require("utf8")
 local _utf8 = {}
 
 _utf8.validate = function(s, c)
-    local size = #s
-    local buffer = ffi.new("char[?]", size, s)
-    local ptr = ffi.cast("char*", buffer)
+	c = c or "?"
+	local size = #s
+	local buffer = ffi.new("char[?]", size, s)
+	local ptr = ffi.cast("char*", buffer)
 
-    local len, pos = utf8.len(s)
-    while not len do
-        ptr[pos - 1] = c:byte()
-        ptr = ptr + pos
-        s = s:sub(pos + 1)
-        len, pos = utf8.len(s)
-    end
+	local len, pos = utf8.len(s)
+	while not len do
+		ptr[pos - 1] = c:byte()
+		ptr = ptr + pos
+		s = s:sub(pos + 1)
+		len, pos = utf8.len(s)
+	end
 
-    return ffi.string(buffer, size)
+	return ffi.string(buffer, size)
 end
 
 return _utf8
