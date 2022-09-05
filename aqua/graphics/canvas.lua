991c26909bf6aa8e78484531f77bc815d827a581
semyon422 <semyon422@users.noreply.github.com>
2022-09-05 21:40:08 +0500

    Use msaa from window mode

--- aqua/graphics/canvas.lua
+++ aqua/graphics/canvas.lua
@@ -1,15 +1,20 @@
 local canvases = {}
 
+local function newCanvas(w, h)
+	local _, _, flags = love.window.getMode()
+	return love.graphics.newCanvas(w, h, {msaa = flags.msaa})
+end
+
 return function(key)
 	local w, h = love.graphics.getDimensions()
 	if not canvases[key] then
-		canvases[key] = love.graphics.newCanvas(w, h)
+		canvases[key] = newCanvas(w, h)
 		return canvases[key]
 	end
 	local canvas = canvases[key]
 	if canvas:getWidth() ~= w or canvas:getHeight() ~= h then
 		canvas:release()
-		canvases[key] = love.graphics.newCanvas(w, h)
+		canvases[key] = newCanvas(w, h)
 	end
 	return canvases[key]
 end
