17c41fdf58e29eebdf8454e4235114c66f4986ed
semyon422 <semyon422@users.noreply.github.com>
2022-09-23 20:04:04 +0500

    Refactor video module

--- aqua/video/Video.lua
+++ aqua/video/Video.lua
@@ -1,5 +1,4 @@
 local ffi = require("ffi")
-local Timer = require("aqua.util.Timer")
 local Class = require("aqua.util.Class")
 local ffmpeg = require("aqua.video.ffmpeg")
 
@@ -10,17 +9,6 @@ local swscale = ffmpeg.swscale
 
 local Video = Class:new()
 
-Video.construct = function(self)
-	self.timer = Timer:new()
-	self.videoTime = 0
-	self.isExternalTimer = false
-end
-
-Video.setTimer = function(self, timer)
-	self.timer = timer
-	self.isExternalTimer = true
-end
-
 Video.load = function(self, path)
 	self.formatContext = ffi.new("AVFormatContext*[1]")
 	self.formatContext[0] = avformat.avformat_alloc_context()
@@ -30,7 +18,7 @@ Video.load = function(self, path)
 
 	assert(avformat.avformat_find_stream_info(self.formatContext[0], nil) == 0)
 
-	self.codec = ffi.new('const AVCodec*[1]')
+	self.codec = ffi.new("const AVCodec*[1]")
 	self.streamIndex = avformat.av_find_best_stream(
 		self.formatContext[0], "AVMEDIA_TYPE_VIDEO", -1, -1, self.codec, 0
 	)
@@ -44,12 +32,8 @@ Video.load = function(self, path)
 
 	local codecContext = self.codecContext
 
-	self.frame = avutil.av_frame_alloc()
-	self.frameRGB = avutil.av_frame_alloc()
-	assert(self.frame)
-	assert(self.frameRGB)
-	ffi.gc(self.frame, avutil.av_free)
-	ffi.gc(self.frameRGB, avutil.av_free)
+	self.frame = ffi.gc(avutil.av_frame_alloc(), avutil.av_free)
+	self.frameRGB = ffi.gc(avutil.av_frame_alloc(), avutil.av_free)
 
 	self.imageData = love.image.newImageData(codecContext.width, codecContext.height)
 	self.image = love.graphics.newImage(self.imageData)
@@ -64,7 +48,7 @@ Video.load = function(self, path)
 		1
 	)
 
-	self.sws_ctx = swscale.sws_getContext(
+	self.swsContext = ffi.gc(swscale.sws_getContext(
 		codecContext.width,
 		codecContext.height,
 		codecContext.pix_fmt,
@@ -75,67 +59,30 @@ Video.load = function(self, path)
 		nil,
 		nil,
 		nil
-	)
-	ffi.gc(self.sws_ctx, swscale.sws_freeContext)
-end
-
-Video.unload = function(self) end
-
-Video.play = function(self)
-	if self.isExternalTimer then
-		return
-	end
-	self.timer:play()
-end
-
-Video.pause = function(self)
-	if self.isExternalTimer then
-		return
-	end
-	self.timer:pause()
-end
-
-Video.setRate = function(self, rate)
-	if self.isExternalTimer then
-		return
-	end
-	self.timer:setRate(rate)
+	), swscale.sws_freeContext)
 end
 
 Video.getTime = function(self)
-	local effortts = self.frame.best_effort_timestamp
-	local timeBase = self.stream.time_base
+	local effort = self.frame.best_effort_timestamp
+	local base = self.stream.time_base
 
-	if effortts < 0 then return 0 end
-	if not self.videoStartTime then
-		self.videoStartTime = effortts
+	if effort < 0 then return 0 end
+	if not self.startTime then
+		self.startTime = effort
 	end
-	if effortts >= self.videoStartTime then
-		return tonumber(effortts - self.videoStartTime)
-			/ timeBase.den * timeBase.num
-	end
-
-	return self.videoTime
+	return tonumber(effort - self.startTime) / base.den * base.num
 end
 
 local packet = ffi.new("AVPacket[1]")
 Video.readFrame = function(self)
 	while avformat.av_read_frame(self.formatContext[0], packet) == 0 do
 		if packet[0].stream_index == self.streamIndex then
-			self.videoTime = self:getTime()
-
-			avcodec.avcodec_send_packet(
-				self.codecContext,
-				packet
-			)
-			local ret = avcodec.avcodec_receive_frame(
-				self.codecContext,
-				self.frame
-			)
+			avcodec.avcodec_send_packet(self.codecContext, packet)
 			avcodec.av_packet_unref(packet)
-			if ret >= 0 then
+
+			if avcodec.avcodec_receive_frame(self.codecContext, self.frame) == 0 then
 				swscale.sws_scale(
-					self.sws_ctx,
+					self.swsContext,
 					ffi.cast("const uint8_t * const *", self.frame.data),
 					self.frame.linesize,
 					0,
@@ -149,32 +96,16 @@ Video.readFrame = function(self)
 		end
 		avcodec.av_packet_unref(packet)
 	end
-
-	return false
 end
 
 Video.rewind = function(self)
 	avformat.av_seek_frame(self.formatContext[0], self.streamIndex, 0, 1)
-
-	if self.image.refresh then
-		self.image:refresh()
-	else
-		self.image:replacePixels(self.imageData)
-	end
+	self.image:replacePixels(self.imageData)
 end
 
-Video.update = function(self, dt)
-	if not self.isExternalTimer then
-		self.timer:update(dt)
-	end
-
-	repeat until not (self.timer:getTime() >= self.videoTime and self:readFrame())
-
-	if self.image.refresh then
-		self.image:refresh()
-	else
-		self.image:replacePixels(self.imageData)
-	end
+Video.seek = function(self, time)
+	repeat until not (time >= self:getTime() and self:readFrame())
+	self.image:replacePixels(self.imageData)
 end
 
 return Video
