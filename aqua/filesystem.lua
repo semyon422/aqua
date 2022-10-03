5bd1a109e4a0a2e754d294108c8fc140c645fd40
semyon422 <semyon422@users.noreply.github.com>
2022-10-03 18:30:05 +0500

    Refactor filesystem module

--- aqua/filesystem.lua
+++ aqua/filesystem.lua
@@ -1,56 +1,38 @@
-local ffi = require('ffi')
-local liblove = ffi.os == 'Windows' and ffi.load('love') or ffi.C
+local ffi = require("ffi")
+local C = ffi.os == "Windows" and ffi.load("love") or ffi.C
 
 ffi.cdef([[
 int PHYSFS_mount(const char *newDir, const char *mountPoint, int appendToPath);
 int PHYSFS_unmount(const char *oldDir);
 int PHYSFS_setWriteDir(const char *newDir);
 const char *PHYSFS_getLastError(void);
-
-typedef unsigned char PHYSFS_uint8;
-typedef struct PHYSFS_Version
-{
-	PHYSFS_uint8 major;
-	PHYSFS_uint8 minor;
-	PHYSFS_uint8 patch;
-} PHYSFS_Version;
-void PHYSFS_getLinkedVersion(PHYSFS_Version *ver);
 ]])
 
-local filesystem = {}
+local physfs = {}
 
-filesystem.version = function()
-	local version = ffi.new("PHYSFS_Version[1]")
-	liblove.PHYSFS_getLinkedVersion(version)
-	return ("%d.%d.%d"):format(version[0].major, version[0].minor, version[0].patch)
-end
-
-filesystem.lastError = function(arg)
-	return error(("%s: %s"):format(ffi.string(liblove.PHYSFS_getLastError()), arg))
-end
-
-filesystem.setWriteDir = function(path)
-	local out = liblove.PHYSFS_setWriteDir(path)
-	if out == 0 then
-		return filesystem.lastError(path)
+function physfs.getLastError()
+	local ptr = C.PHYSFS_getLastError()
+	if ptr == nil then  -- cdata<const char *>: NULL
+		return
 	end
-	return out
+	return ffi.string(ptr)
 end
 
-filesystem.mount = function(newDir, mountPoint, appendToPath)
-	local out = liblove.PHYSFS_mount(newDir, mountPoint, appendToPath)
-	if out == 0 then
-		return filesystem.lastError(newDir)
-	end
-	return out
+local function protect(res)
+	if res then return res end
+	return nil, physfs.getLastError()
 end
 
-filesystem.unmount = function(oldDir)
-	local out = liblove.PHYSFS_unmount(oldDir)
-	if out == 0 then
-		return filesystem.lastError(oldDir)
-	end
-	return out
+function physfs.setWriteDir(path)
+	return protect(C.PHYSFS_setWriteDir(path) ~= 0)
 end
 
-return filesystem
+function physfs.mount(newDir, mountPoint, appendToPath)
+	return protect(C.PHYSFS_mount(newDir, mountPoint, appendToPath) ~= 0)
+end
+
+function physfs.unmount(oldDir)
+	return protect(C.PHYSFS_unmount(oldDir) ~= 0)
+end
+
+return physfs
