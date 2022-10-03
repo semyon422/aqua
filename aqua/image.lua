85c2b2f291550e5ef6e7a48157cbd7efe3d62fd9
semyon422 <semyon422@users.noreply.github.com>
2022-10-03 14:58:30 +0500

    Refactor image module

--- aqua/image.lua
+++ aqua/image.lua
@@ -2,42 +2,19 @@ local aquathread = require("aqua.thread")
 
 local image = {}
 
-local ImageData = {}
-
-ImageData.release = function(self)
-	self.imageData:release()
-	self.image:release()
-end
-
-image.newImageData = function(s)
+local newImageDataAsync = aquathread.async(function(s)
 	require("love.image")
 	local status, err = pcall(love.image.newImageData, s)
 	if not status then
-		return
+		return status, err
 	end
-
-	local imageData = setmetatable({}, {__index = ImageData})
-	imageData.imageData = err
-
-	if love.graphics then
-		imageData.image = love.graphics.newImage(imageData.imageData)
-	end
-
-	return imageData
-end
-
-local newImageDataAsync = aquathread.async(function(s)
-	local image = require("aqua.image")
-	return image.newImageData(s)
+	return err
 end)
 
-image.newImageDataAsync = function(s)
+image.newImageAsync = function(s)
 	local imageData = newImageDataAsync(s)
 	if not imageData then return end
-	if not imageData.image then
-		imageData.image = love.graphics.newImage(imageData.imageData)
-	end
-	return setmetatable(imageData, {__index = ImageData})
+	return love.graphics.newImage(imageData)
 end
 
 return image
