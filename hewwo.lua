130238f392f2deba2407c1fb46ed33918792aeed
semyon422 <semyon422@users.noreply.github.com>
2022-10-25 00:08:50 +0500

    Create hewwo.lua

---
+++ hewwo.lua
@@ -0,0 +1,25 @@
+-- based on https://github.com/SayakaIsBaka/hewwo-node
+
+local hewwo = {}
+
+local kaomojis = {"(・`ω´・)", ";;w;;", "owo", "UwU", ">w<", "^w^"}
+
+local gsubs = {
+	{"r", "w"},
+	{"l", "w"},
+	{"R", "W"},
+	{"L", "W"},
+	{"ove", "uv"},
+	{"([nN])([aeiou])", "%1y%2"},
+	{"!+", function() return " " .. kaomojis[math.random(1, #kaomojis)] end},
+}
+
+function hewwo.hewwoify(s)
+	for i = 1, #gsubs do
+		s = s:gsub(unpack(gsubs[i]))
+	end
+
+	return s
+end
+
+return hewwo
