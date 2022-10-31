b0865179a00878ae64cb2f73bc96d9ab8c859322
semyon422 <semyon422@users.noreply.github.com>
2022-10-31 20:00:06 +0500

    Create dpairs.lua

---
+++ dpairs.lua
@@ -0,0 +1,24 @@
+local function sort(a, b)
+	local ta, tb = type(a), type(b)
+	local na, nb = ta == "number", tb == "number"
+	if na and nb then
+		return a < b
+	elseif na or nb then
+		return na and not nb
+	end
+	return tostring(a) < tostring(b)
+end
+
+return function(t)
+	local _t = {}
+	for k in pairs(t) do
+		table.insert(_t, k)
+	end
+	table.sort(_t, sort)
+
+	local i = 0
+	return function()
+		i = i + 1
+		return t[_t[i]]
+	end
+end
