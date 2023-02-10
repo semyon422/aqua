294937f72d825cc884494d5a95894d0b4bf671d0
semyon422 <semyon422@users.noreply.github.com>
2023-02-10 20:30:13 +0500

    Better math.belong

--- math_util.lua
+++ math_util.lua
@@ -4,20 +4,23 @@ local abs = math.abs
 
 local math = {}
 
-math.round = function(x, to)
+function math.round(x, to)
 	to = to or 1
 	return ((x / to) % 1 < 0.5 and floor(x / to) or ceil(x / to)) * to
 end
 
-math.sign = function(x)
+function math.sign(x)
 	return x == 0 and 0 or x / abs(x)
 end
 
-math.belong = function(x, a, b)
+function math.belong(x, a, b)
+	if b < a then
+		a, b = b, a
+	end
 	return a <= x and x <= b
 end
 
-math.map = function(x, a, b, c, d)
+function math.map(x, a, b, c, d)
 	return (x - a) * (d - c) / (b - a) + c
 end
 
