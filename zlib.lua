36047024e6ab0b3c29d165ba594f821647f67f1f
semyon422 <semyon422@users.noreply.github.com>
2023-03-24 23:15:02 +0500

    Fix zlib types

--- zlib.lua
+++ zlib.lua
@@ -11,13 +11,13 @@ unsigned long compressBound(unsigned long sourceLen);
 local zlib = {}
 
 function zlib.uncompress(dst_p, dst_size, src_p, src_size)
-	local out_size = ffi.new("size_t[1]", dst_size)
+	local out_size = ffi.new("unsigned long[1]", dst_size)
 	assert(_zlib.uncompress(dst_p, out_size, src_p, src_size) == 0)
 	return tonumber(out_size[0])
 end
 
 function zlib.compress(dst_p, dst_size, src_p, src_size, level)
-	local out_size = ffi.new("size_t[1]", dst_size)
+	local out_size = ffi.new("unsigned long[1]", dst_size)
 	assert(_zlib.compress2(dst_p, out_size, src_p, src_size, level or -1) == 0)
 	return tonumber(out_size[0])
 end
