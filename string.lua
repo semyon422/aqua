5f7fff6fdd03e92bbc9af96bea1acf967037c6e9
semyon422 <semyon422@users.noreply.github.com>
2023-04-12 19:40:33 +0500

    Add string.isplit stateless split iterator

--- string.lua
+++ string.lua
@@ -1,18 +1,38 @@
-string.trim = function(s)
+local table_util = require("table_util")
+
+function string.trim(s)
 	return s:match("^%s*(.-)%s*$")
 end
 
-string.split = function(s, divider, plain)
+function string.split(s, div)
 	local out = {}
 
 	local pos = 0
-	for a, b in function() return s:find(divider, pos, plain) end do
+	for a, b in function() return s:find(div, pos, true) end do
 		out[#out + 1] = s:sub(pos, a - 1)
 		pos = b + 1
 	end
 	out[#out + 1] = s:sub(pos)
 
 	return out
+end
+
+local function next_split(d)
+	return function(s, p)
+		if not p then
+			return
+		end
+		local a, b = s:find(d, p, true)
+		if not a then
+			return false, s:sub(p)
+		end
+		return b + 1, s:sub(p, a - 1)
+	end
+end
+next_split = table_util.cache(next_split)
+
+function string.isplit(s, d)
+	return next_split(d), s, 1
 end
 
 return string
