aad7a28f8d043a7aace12618f980a9fc2d056ce4
semyon422 <semyon422@users.noreply.github.com>
2023-05-11 20:40:42 +0500

    Don't call constructor on class inheritance

--- class_new.lua
+++ class_new.lua
@@ -1,14 +1,15 @@
+local extend = {}
+
 ---@param f function? Parent constructor
----@param ... any Parent constructor arguments
 ---@return table T Class table
 ---@return fun(...: any): table new Class constructor
 ---@return table mt Class metatable
-return function(f, ...)
-	local T = f and f() or {}
+return function(f)
+	local T = f and f(extend) or {}
 	local mt = {__index = T}
 	return T, function(...)
 		local t = setmetatable({}, mt)
-		if T.new then t:new(...) end
+		if T.new and ... ~= extend then t:new(...) end
 		return t
 	end, mt
 end
