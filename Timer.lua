273e366617e2f9971f0b041e1280c55a0698a949
semyon422 <semyon422@users.noreply.github.com>
2023-07-17 22:41:54 +0500

    Reimplement Timer class

--- Timer.lua
+++ Timer.lua
@@ -2,130 +2,77 @@ local Class = require("Class")
 
 local Timer = Class:new()
 
-local love = love
+Timer.isPlaying = false
+Timer.offset = 0
+Timer.rate = 1
+Timer.adjustRate = 0.1
 
-Timer.construct = function(self)
-	self:reset()
-end
-
-Timer.reset = function(self)
-	self.startTime = self:getAbsoluteTime()
-	self.pauseStartTime = self.startTime
-	self.isPlaying = false
-	self.currentTime = 0
-	self.deltaTime = 0
-	self.rate = 1
-	self.currentTime = 0
-	self.pauseTime = 0
-	self.adjustDelta = 0
-	self.rateDelta = 0
-	self.positionDelta = 0
-end
-
-Timer.getAbsoluteTime = function(self)
-	if love then
-		return love.timer.getTime()
-	end
-	return os.time()
-end
-
-Timer.getAbsoluteDelta = function(self)
-	if love then
-		return love.timer.getDelta()
-	end
+function Timer:getAbsoluteTime()
 	return 0
 end
 
-Timer.update = function(self)
-	self.deltaTime = self:getAbsoluteTime() - self.startTime
-
-	if not self.isPlaying then
+function Timer:tryAdjust()
+	if not self.getAdjustTime then
 		return
 	end
-	self.currentTime = (self.deltaTime - self.adjustDelta - self.pauseTime - self.rateDelta) * self.rate + self.positionDelta
-
-	if self.getAdjustTime then
-		self:adjustTime()
-	end
-end
-
-Timer.adjustTime = function(self, force)
 	local adjustTime = self:getAdjustTime()
-	if not adjustTime or not self.isPlaying then
+	if not adjustTime then
 		return
 	end
-
-	local dt = math.min(self:getAbsoluteDelta(), 1 / 60)
-	local targetAdjustDelta
-		= self.deltaTime
-		- self.rateDelta
-		- self.pauseTime
-		- (adjustTime - self.positionDelta)
-		/ self.rate
-
-	if force then
-		self.adjustDelta = targetAdjustDelta
+	if adjustTime == self.prevAdjustTime then
 		return
 	end
-
-	self.adjustDelta = self.adjustDelta + (targetAdjustDelta - self.adjustDelta) * dt
+	self.prevAdjustTime = adjustTime
+	return adjustTime
 end
 
-Timer.setRate = function(self, rate)
-	local pauseTime = self.pauseTime
-	local time = self:getAbsoluteTime()
+function Timer:getTime()
 	if not self.isPlaying then
-		pauseTime = pauseTime + time - self.pauseStartTime
-	end
-	local deltaTime = time - self.startTime - pauseTime
-	self.rateDelta = (self.rateDelta - deltaTime) * self.rate / rate + deltaTime
-
-	local adjust = false
-	if rate * self.rate < 0 then
-		if self.getAdjustTime then
-			adjust = true
-		end
+		return self.offset
 	end
 
+	local dt = self:getAbsoluteTime() - self.startTime
+	local time = dt * self.rate + self.offset
+
+	local adjustTime = self:tryAdjust()
+	if adjustTime and self.adjustRate > 0 then
+		time = time + (adjustTime - time) * self.adjustRate
+		self:setTime(time)
+	end
+
+	return time
+end
+
+function Timer:transform(time)
+	return time - self:getAbsoluteTime() + self:getTime()
+end
+
+function Timer:setTime(time)
+	self.offset = time or self:getTime()
+	self.startTime = self:getAbsoluteTime()
+end
+
+function Timer:setRate(rate)
+	self:setTime()
 	self.rate = rate
-
-	if adjust then
-		self:adjustTime(true)
-	end
 end
 
-Timer.getTime = function(self)
-	return self.currentTime
-end
-
-Timer.transformTime = function(self, eventTime)
-	return eventTime - self:getAbsoluteTime() + self:getTime()
-end
-
-Timer.setPosition = function(self, position)
-	local currentTime = self.currentTime - self.positionDelta
-	self.positionDelta = position - currentTime
-	self.currentTime = currentTime + self.positionDelta
-end
-
-Timer.pause = function(self)
+function Timer:pause()
 	if not self.isPlaying then
 		return
 	end
 
+	self:setTime()
 	self.isPlaying = false
-	self.pauseStartTime = self:getAbsoluteTime()
 end
 
-Timer.play = function(self)
+function Timer:play()
 	if self.isPlaying then
 		return
 	end
 
-	local time = self:getAbsoluteTime()
+	self:setTime()
 	self.isPlaying = true
-	self.pauseTime = self.pauseTime + time - self.pauseStartTime
-	self.pauseStartTime = time
 end
 
 return Timer
---
+++ tests/Timer.lua
@@ -0,0 +1,81 @@
+local Timer = require("Timer")
+
+local time
+function Timer:getAbsoluteTime()
+	return time
+end
+
+do
+	time = 0
+	local timer = Timer:new()
+
+	timer:play()
+
+	assert(timer:getTime() == 0)
+
+	time = 1
+
+	assert(timer:getTime() == 1)
+
+	timer:setRate(2)
+	time = 2
+
+	assert(timer:getTime() == 3)
+
+	timer:setTime(2)
+
+	assert(timer:getTime() == 2)
+
+	timer:setRate(0.5)
+	time = 3
+
+	assert(timer:getTime() == 2.5)
+
+	timer:pause()
+	time = 4
+
+	assert(timer:getTime() == 2.5)
+
+	timer:play()
+
+	assert(timer:getTime() == 2.5)
+
+	time = 5
+
+	assert(timer:getTime() == 3)
+end
+
+do
+	time = 0
+	local adjTime = 0
+
+	local timer = Timer:new()
+	timer.adjustRate = 1
+	function timer:getAdjustTime()
+		return adjTime
+	end
+
+	timer:play()
+
+	assert(timer:getTime() == 0)
+
+	time = 1
+	adjTime = 1
+
+	assert(timer:getTime() == 1)
+
+	time = 2
+	adjTime = 2
+
+	assert(timer:getTime() == 2)
+
+	time = 2
+	adjTime = 3
+
+	assert(timer:getTime() == 3)
+
+	time = 3
+	adjTime = 3
+
+	assert(timer:getTime() == 4)
+end
