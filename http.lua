dc40ec1324bca255d2535eaad85ceae119b0071e
semyon422 <semyon422@users.noreply.github.com>
2023-07-26 17:10:16 +0500

    Try https first

--- http.lua
+++ http.lua
@@ -1,19 +1,13 @@
 local https = require("ssl.https")
 local socket_http = require("socket.http")
-local socket_url = require("socket.url")
 
 local http = {}
 
 function http.request(url, body)
-	local string_url = url
-	if type(url) ~= "string" then
-		string_url = url.url
-	end
+	local q, w, e, r = https.request(url, body)
 
-	local parsed = socket_url.parse(string_url)
-
-	if parsed.scheme == "https" then
-		return https.request(url, body)
+	if q then
+		return q, w, e, r
 	end
 
 	return socket_http.request(url, body)
