e71f5f702e90d4e5c34f24c0fc83974274e8c48f
semyon422 <semyon422@users.noreply.github.com>
2023-08-11 19:48:37 +0500

    Add typeof to class library

--- class_new2.lua
+++ class_new2.lua
@@ -13,16 +13,49 @@ local function new(T, ...)
 	return return_from_new(t, t:new(...))
 end
 
+local function isclass(T)
+	if type(T) ~= "table" then
+		return false
+	end
+
+	local mt = getmetatable(T)
+	return mt and mt.__call == new
+end
+
+local function typeof(T, t)
+	if type(t) ~= "table" then
+		return false
+	end
+
+	local _T = t
+	if not isclass(t) then
+		_T = getmetatable(t)
+	end
+	if _T == T then
+		return true
+	end
+
+	while _T do
+		local p = getmetatable(_T).__index
+		if p == T then
+			return true
+		end
+		_T = p
+	end
+
+	return false
+end
+
 local function class(p, t)
 	if p then
-		local mt = getmetatable(p)
-		assert(mt and mt.__call == new, "bad argument #1 to 'class'")
+		assert(isclass(p), "bad argument #1 to 'class'")
 	end
 
 	local mt = {
+		__index = p,
 		__call = new,
 		__add = class,
-		__index = p,
+		__mul = typeof,
 	}
 
 	local T = t or {}
@@ -30,5 +63,18 @@ local function class(p, t)
 
 	return setmetatable(T, mt)
 end
+
+-- tests
+
+local A = class()
+local B = A + {}
+
+local b = B()
+
+assert(B * b)
+assert(A * b)
+
+assert(B * B)
+assert(A * B)
 
 return class
