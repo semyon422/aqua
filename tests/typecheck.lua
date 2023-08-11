ffcb5d46eba4e2260fb890644e4e01196d7ade12
semyon422 <semyon422@users.noreply.github.com>
2023-08-11 19:39:21 +0500

    Add typecheck library and tests

---
+++ tests/typecheck.lua
@@ -0,0 +1,291 @@
+local typecheck = require("typecheck")
+local class = require("class")
+
+local function lex(...)
+	return assert(typecheck.lex(...))
+end
+
+local function typeof(t, T)
+	return T * t
+end
+
+local function check_types(...)
+	local res, i, _type, got = typecheck.check_types(...)
+	if res then
+		return res
+	end
+	return nil, ("bad argument #%s (%s expected, got %s)"):format(i, _type, got)
+end
+
+do
+	local tokens = lex("a ( ) ... [] : . , ? |")
+	assert(#tokens == 10)
+end
+
+do
+	local tokens = lex("a.b.c")
+	local name = tokens:parse_name_novararg()
+	assert(tokens.pos == #tokens + 1)
+	assert(name == "a.b.c")
+end
+
+do
+	local tokens = lex("a.b.")
+	local name, err = tokens:parse_name_novararg()
+	assert(err == "token expected")
+end
+
+do
+	local tokens = lex("a.?")
+	local name, err = tokens:parse_name_novararg()
+	assert(err == "unexpected token '?' at position 3")
+end
+
+do
+	local tokens = lex("...")
+	local name, err = tokens:parse_name_novararg()
+	assert(err == "unexpected token '...' at position 1")
+end
+
+do
+	local tokens = lex("...")
+	local name = tokens:parse_name()
+	assert(name == "...")
+end
+
+do
+	local tokens = lex("number")
+	local t = tokens:parse_type()
+	assert(typeof(t, typecheck.Type))
+	assert(tostring(t) == "number")
+end
+
+do
+	local tokens = lex("(number")
+	local t, err = tokens:parse_type()
+	assert(err == "token expected")
+end
+
+do
+	local tokens = lex("(number...")
+	local t, err = tokens:parse_type()
+	assert(err == "unexpected token '...' at position 8")
+end
+
+do
+	local tokens = lex("[][]number")
+	local t = assert(tokens:parse_type())
+	assert(typeof(t, typecheck.ArrayType))
+	assert(typeof(t.type, typecheck.ArrayType))
+	assert(typeof(t.type.type, typecheck.Type))
+	assert(tostring(t) == "[][]number")
+end
+
+do
+	local tokens = lex("number, string")
+	local types = assert(tokens:parse_types())
+	assert(#types == 2)
+	assert(typeof(types[1], typecheck.Type))
+	assert(typeof(types[2], typecheck.Type))
+end
+
+do
+	local tokens = lex("number, string...")
+	local types = assert(tokens:parse_types())
+	assert(#types == 2)
+	assert(types.is_vararg)
+end
+
+do
+	local tokens = lex("string?")
+	local t = assert(tokens:parse_type_union())
+	assert(#t == 1)
+	assert(t.is_optional)
+	assert(typeof(t, typecheck.UnionType))
+	assert(tostring(t) == "string?")
+	assert(tostring(t[1]) == "string")
+end
+
+do
+	local tokens = lex("()")
+	local t, err = tokens:parse_type_union()
+	assert(err == "unexpected token ')' at position 2")
+end
+
+do
+	local tokens = lex("(number)")
+	local t = assert(tokens:parse_type_union())
+	assert(not tokens.token)
+end
+
+do
+	local tokens = lex("(number|string?)")
+	local t = assert(tokens:parse_type_union())
+	assert(t.is_optional)
+	assert(typeof(t, typecheck.UnionType))
+end
+
+do
+	local tokens = lex("[](number|string)")
+	local t = assert(tokens:parse_type_union())
+	assert(typeof(t, typecheck.ArrayType))
+	assert(typeof(t.type, typecheck.UnionType))
+	assert(typeof(t.type[1], typecheck.Type))
+	assert(typeof(t.type[2], typecheck.Type))
+	assert(t.type[1].type == "number")
+	assert(t.type[2].type == "string")
+end
+
+do
+	local tokens = lex("(number|string)|(number|string)")
+	local t = assert(tokens:parse_type_union())
+end
+
+do
+	local tokens = lex("(number|(number|(number|(string))))")
+	local t = assert(tokens:parse_type_union())
+end
+
+do
+	local tokens = lex("[]([](string|number)|number)")
+	local t = assert(tokens:parse_type_union())
+end
+
+--------------------------------------------------------------------------------
+
+do
+	local tokens = lex("a: number")
+	local param_name, param_type = tokens:parse_param()
+	assert(param_name == "a")
+	assert(typeof(param_type, typecheck.Type))
+end
+
+do
+	local tokens = lex("...: number")
+	local param_name, param_type, is_vararg = tokens:parse_param()
+	assert(param_name == "...")
+	assert(is_vararg)
+	assert(typeof(param_type, typecheck.Type))
+end
+
+do
+	local tokens = lex("a:")
+	local param_name, param_type, is_vararg = tokens:parse_param()
+	assert(param_type == "token expected")
+end
+
+do
+	local tokens = lex("a: ?")
+	local param_name, param_type, is_vararg = tokens:parse_param()
+	assert(param_type == "unexpected token '?' at position 4")
+end
+
+do
+	local tokens = lex("(a: number, b: string)")
+	local param_names, param_types = tokens:parse_params()
+	assert(#param_names == 2)
+end
+
+do
+	local tokens = lex("(a: number, ...: string)")
+	local param_names, param_types = tokens:parse_params()
+	assert(param_names and param_types)
+	assert(#param_names == 2)
+	assert(param_types.is_vararg)
+end
+
+do
+	local tokens = lex("(...: number...)")
+	local param_names, param_types = tokens:parse_params()
+	assert(param_types == "unexpected token '...' at position 13")
+end
+
+do
+	local tokens = lex("(...: number, ...: number)")
+	local param_names, param_types = tokens:parse_params()
+	assert(param_types == "unexpected token ',' at position 13")
+end
+
+local function assert_parse_types(s, ...)
+	local tokens = lex(s)
+	local types = tokens:parse_types()
+	assert(check_types(types, ...))
+end
+
+local function assert_not_parse_types(s, ...)
+	local tokens = lex(s)
+	local types = tokens:parse_types()
+	assert(not check_types(types, ...))
+end
+
+assert_parse_types("number, string", 1, "q")
+
+assert_not_parse_types("number")
+assert_parse_types("number", 1)
+assert_parse_types("number", 1, 2, 3)
+
+assert_not_parse_types("number|nil")
+assert_parse_types("number|nil", nil)
+assert_parse_types("number|nil", 1)
+
+-- t? = t | nil | no value
+
+assert_parse_types("number?")
+assert_parse_types("number?", nil)
+assert_parse_types("number?", 1)
+
+-- t... = t | no value
+
+assert_parse_types("number...")
+assert_parse_types("number...", 1)
+assert_parse_types("number...", 1, 2)
+assert_not_parse_types("number...", nil)
+assert_not_parse_types("number...", 1, nil)
+
+-- t?... = t | nil | no value, t?...
+
+assert_parse_types("number?...")
+assert_parse_types("number?...", nil)
+assert_parse_types("number?...", 1, nil)
+
+assert_not_parse_types("any")
+assert_parse_types("any", 1)
+assert_parse_types("any", "q")
+
+assert_not_parse_types("any", nil)
+assert_parse_types("any?", nil)
+assert_parse_types("any...")
+assert_not_parse_types("any...", nil)
+assert_parse_types("any?...", nil)
+
+do
+	local MyClass = class()
+	local obj = MyClass()
+
+	assert_not_parse_types("MyClass", obj)
+	typecheck.register_class("MyClass", MyClass)
+	assert_parse_types("MyClass", obj)
+end
+
+do
+	local tokens = lex("number string")
+	local types = tokens:parse_types()
+	assert(#types == 1)
+end
+
+assert(typecheck.parse_def("mylib.mul(): number"))
+assert(typecheck.parse_def("()"))
+
+do
+	local def = "mod.test(a: number|string?, b: []MyClass, ...: function): [][]number, string|number?, MyClass, table..."
+
+	local MyClass = class()
+	typecheck.register_class("MyClass", MyClass)
+
+	local function test(a, b, ...)
+		return {{1}}, nil, MyClass()
+	end
+	test = typecheck.decorate(test, def)
+
+	test(nil, {MyClass()})
+end
---
+++ typecheck.lua
@@ -0,0 +1,585 @@
+local class = require("class")
+
+local typecheck = {}
+
+---@class typecheck.Type
+---@field type string
+---@operator call: typecheck.Type
+local Type = class()
+typecheck.Type = Type
+
+function Type:new(_type)
+	self.type = _type
+end
+
+function Type:check(value)
+	return type(value) == self.type
+end
+
+function Type.__tostring(t)
+	return t.type
+end
+
+---@class typecheck.AnyType: typecheck.Type
+---@operator call: typecheck.AnyType
+local AnyType = class()
+typecheck.AnyType = AnyType
+
+function AnyType:check(value)
+	return value ~= nil
+end
+
+function AnyType.__tostring()
+	return "any"
+end
+
+---@class typecheck.ClassType: typecheck.Type
+---@operator call: typecheck.ClassType
+local ClassType = class()
+typecheck.ClassType = ClassType
+
+function ClassType:new(name, T)
+	self.name = name
+	self.type = T
+end
+
+function ClassType:check(value)
+	return self.type * value
+end
+
+function ClassType.__tostring(t)
+	return t.name
+end
+
+---@class typecheck.ArrayType: typecheck.Type
+---@field type typecheck.Type
+---@operator call: typecheck.ArrayType
+local ArrayType = class(Type)
+typecheck.ArrayType = ArrayType
+
+function ArrayType:check(value)
+	return not value[1] or self.type:check(value[1])
+end
+
+function ArrayType.__tostring(t)
+	return "[]" .. tostring(t.type)
+end
+
+---@class typecheck.UnionType: typecheck.Type
+---@field is_optional boolean
+---@operator call: typecheck.UnionType
+local UnionType = class(Type)
+typecheck.UnionType = UnionType
+
+function UnionType:check(value)
+	if value == nil and self.is_optional then
+		return true
+	end
+	for _, _type in ipairs(self) do
+		if _type:check(value) then
+			return true
+		end
+	end
+	return false
+end
+
+function UnionType.__tostring(t)
+	local out = {}
+	for i, v in ipairs(t) do
+		out[i] = tostring(v)
+	end
+	local s = table.concat(out, "|")
+	if t.is_optional then
+		s = s .. "?"
+	end
+	return s
+end
+
+--------------------------------------------------------------------------------
+
+---@class typecheck.Token
+---@field pos integer
+---@field type string
+---@field value string
+---@operator call: typecheck.Token
+local Token = class()
+
+function Token:new(type, value, pos)
+	self.type = type
+	self.value = value
+	self.pos = pos
+end
+
+function Token.__tostring(t)
+	return ("%s %s %s"):format(t.pos, t.type, t.value)
+end
+
+--------------------------------------------------------------------------------
+
+local parse_error = "unexpected token '%s' at position %s"
+local function get_token_error(token)
+	if not token then
+		return "token expected"
+	end
+	return parse_error:format(token.value, token.pos)
+end
+
+--------------------------------------------------------------------------------
+
+---@class typecheck.Tokens
+---@field pos integer
+---@field stack number[]
+---@field [number] typecheck.Token
+---@operator call: typecheck.Tokens
+local Tokens = class()
+
+function Tokens:new()
+	self.stack = {}
+	self.pos = 1
+end
+
+function Tokens:_push()
+	table.insert(self.stack, self.pos)
+end
+
+function Tokens:_pop(save)
+	if save then
+		table.remove(self.stack)
+		return
+	end
+	local token = self.token
+	self.pos = table.remove(self.stack)
+	self.token = self[self.pos]
+	return token
+end
+
+function Tokens:step()
+	self.pos = self.pos + 1
+	self.token = self[self.pos]
+end
+
+function Tokens:parse_name_novararg()
+	if not self.token then
+		return nil, get_token_error()
+	end
+
+	self:_push()
+
+	local name = ""
+	local next_type = "id"
+	while self.token and self.token.type == next_type do
+		name = name .. self.token.value
+		self:step()
+		next_type = next_type ~= "point" and "point" or "id"
+	end
+
+	if next_type == "id" then
+		return nil, get_token_error(self:_pop())
+	end
+
+	self:_pop(true)
+
+	return name
+end
+
+function Tokens:parse_name()
+	if not self.token then
+		return nil, get_token_error()
+	end
+
+	local token = self.token
+	if token.type == "vararg" then
+		self:step()
+		return token.value
+	end
+
+	return self:parse_name_novararg()
+end
+
+function Tokens:parse_type()
+	if not self.token then
+		return nil, get_token_error()
+	end
+
+	self:_push()
+
+	local array_depth = 0
+	while self.token and self.token.type == "array" do
+		array_depth = array_depth + 1
+		self:step()
+	end
+
+	local _type
+	if self.token.type == "leftparan" then
+		self:step()
+
+		_type = self:parse_type_union()
+		if not _type or not self.token or self.token.type ~= "rightparan" then
+			return nil, get_token_error(self:_pop())
+		end
+
+		self:step()
+	else
+		local t, err = self:parse_name_novararg()
+		if not t then
+			self:_pop()
+			return nil, err
+		end
+		_type = typecheck.get_type(t)
+	end
+
+	for _ = 1, array_depth do
+		_type = ArrayType(_type)
+	end
+
+	self:_pop(true)
+
+	return _type
+end
+
+function Tokens:parse_type_union()
+	if not self.token then
+		return nil, get_token_error()
+	end
+
+	self:_push()
+
+	if self.token.type == "leftparan" then
+		self:step()
+
+		local union = self:parse_type_union()
+		if not union or self.token.type ~= "rightparan" then
+			return nil, get_token_error(self:_pop())
+		end
+
+		self:step()
+		self:_pop(true)
+
+		return union
+	end
+
+	local union = UnionType()
+
+	local final_err
+	local step = "token"
+	while self.pos <= #self do
+		if step == "pipe" and self.token.type == "pipe" then
+			self:step()
+			step = "token"
+		elseif step == "token" then
+			local _type, err = self:parse_type()
+			if not _type then
+				final_err = err
+				break
+			end
+			table.insert(union, _type)
+			step = "pipe"
+		else
+			break
+		end
+	end
+
+	if #union == 0 then
+		local t = self:_pop()
+		return nil, final_err or get_token_error(t)
+	end
+
+	if self.token and self.token.type == "question" then
+		union.is_optional = true
+		self:step()
+	end
+
+	self:_pop(true)
+
+	if #union == 1 and not union.is_optional then
+		return union[1]
+	end
+
+	return union
+end
+
+function Tokens:parse_types()
+	if not self.token then
+		return nil, get_token_error()
+	end
+
+	self:_push()
+
+	local types = {}
+
+	local step = "type"
+	while self.pos <= #self do
+		local token = self.token
+		if step == "comma" and token.type == "comma" then
+			self:step()
+			step = "type"
+		elseif step == "comma" and token.type == "vararg" then
+			types.is_vararg = true
+			self:step()
+			break
+		elseif step == "type" then
+			local _type, err = self:parse_type_union()
+			if not _type then
+				self:_pop()
+				return nil, err
+			end
+			table.insert(types, _type)
+			step = "comma"
+		else
+			break
+		end
+	end
+
+	self:_pop(true)
+
+	return types
+end
+
+function Tokens:parse_param()
+	if not self.token then
+		return nil, get_token_error()
+	end
+
+	self:_push()
+
+	local param_name, err = self:parse_name()
+	if not param_name then
+		self:_pop()
+		return nil, err
+	end
+	if self.token.type ~= "colon" then
+		return nil, get_token_error(self:_pop())
+	end
+
+	self:step()
+
+	local param_type, err = self:parse_type_union()
+	if not param_type then
+		self:_pop()
+		return nil, err
+	end
+
+	self:_pop(true)
+
+	return param_name, param_type, param_name == "..."
+end
+
+function Tokens:parse_params()
+	if not self.token then
+		return nil, get_token_error()
+	end
+
+	self:_push()
+
+	if self.token.type ~= "leftparan" then
+		return nil, get_token_error(self:_pop())
+	end
+	self:step()
+
+	local param_names = {}
+	local param_types = {}
+
+	local expect_rightparan = false
+
+	local step = "param"
+	while self.pos <= #self do
+		local token = self.token
+		if expect_rightparan and token.type ~= "rightparan" then
+			return nil, get_token_error(self:_pop())
+		end
+		if token.type == "rightparan" then
+			self:step()
+			self:_pop(true)
+			return param_names, param_types
+		end
+		if step == "comma" and token.type == "comma" then
+			self:step()
+			step = "param"
+		elseif step == "param" then
+			local param_name, param_type, is_vararg = self:parse_param()
+			if not param_name then
+				self:_pop()
+				return nil, param_type
+			end
+			table.insert(param_names, param_name)
+			table.insert(param_types, param_type)
+			if is_vararg then
+				param_types.is_vararg = is_vararg
+				expect_rightparan = true
+			end
+			step = "comma"
+		else
+			return nil, get_token_error(self:_pop())
+		end
+	end
+
+	self:_pop(true)
+
+	return param_names, param_types
+end
+
+--------------------------------------------------------------------------------
+
+local token_patterns = {
+	{"id", "[%w_]+"},
+	{"leftparan", "%("},
+	{"rightparan", "%)"},
+	{"vararg", "%.%.%."},
+	{"array", "%[%]"},
+	{"colon", ":"},
+	{"point", "%."},
+	{"comma", "%,"},
+	{"question", "%?"},
+	{"pipe", "|"},
+	{"minus", "%-"},
+	{"at", "@"},
+}
+
+---@param s string
+---@return typecheck.Tokens? tokens
+---@return string? error_message
+function typecheck.lex(s)
+	local pos = 1
+	local tokens = Tokens()
+
+	s = s:match("^%s*(.-)%s*$")
+	while pos <= #s do
+		local a, b, token
+		for _, p in ipairs(token_patterns) do
+			a, b, token = s:find("^(" .. p[2] .. ")%s*", pos)
+			if token then
+				table.insert(tokens, Token(p[1], token, pos))
+				break
+			end
+		end
+		if not token then
+			return nil, "unknown symbol '" .. s:sub(pos, pos) .. "' at position " .. pos
+		end
+		pos = b + 1
+	end
+
+	tokens.token = tokens[1]
+
+	return tokens
+end
+
+local class_by_name = {}
+
+function typecheck.register_class(_type, T)
+	class_by_name[_type] = T
+end
+
+function typecheck.get_type(name)
+	if name == "any" then
+		return AnyType()
+	end
+	if class_by_name[name] then
+		return ClassType(name, class_by_name[name])
+	end
+	return Type(name)
+end
+
+function typecheck.parse_def(signature)
+	local def = {
+		name = "?",
+		param_names = {},
+		param_types = {},
+		return_types = {},
+	}
+
+	local tokens, err = typecheck.lex(signature)
+	if not tokens then
+		return nil, err
+	end
+
+	local name, err = tokens:parse_name()
+	if not name then
+		name = "?"
+	end
+	def.name = name
+
+	local param_names, param_types = tokens:parse_params()
+	if not param_names then
+		return nil, param_types
+	end
+
+	def.param_names = param_names
+	def.param_types = param_types
+
+	if not tokens.token then
+		return def
+	end
+	if tokens.token.type ~= "colon" then
+		return nil, get_token_error(tokens.token)
+	end
+
+	tokens:step()
+
+	if not tokens.token then
+		return nil, get_token_error()
+	end
+
+	local return_types, err = tokens:parse_types()
+	if not return_types then
+		return nil, err
+	end
+	def.return_types = return_types
+
+	return def
+end
+
+local function check(types, ...)
+	local n = select("#", ...)
+
+	local iter_to = #types
+	if types.is_vararg then
+		iter_to = math.max(iter_to - 1, n)
+	end
+
+	for i = 1, iter_to do
+		local v = select(i, ...)
+		local got = i <= n and type(v) or "no value"
+		local _type = types[math.min(i, #types)]
+
+		local res = false
+		if i <= n then
+			res = _type:check(v)
+		else  -- no value
+			res = _type.is_optional
+		end
+
+		if not res then
+			return false, i, _type, got
+		end
+	end
+
+	return true, ...
+end
+typecheck.check_types = check
+
+local function wrap_return(err, res, ...)
+	if res then
+		return ...
+	end
+	local i, _type, got = ...
+	error(err:format(i, _type, got), 2)
+end
+
+local n_to_exp_got = "#%s to '?' (%s expected, got %s)"
+
+---@generic T
+---@param f T
+---@param signature string
+---@return T f
+function typecheck.decorate(f, signature)
+	local s = assert(typecheck.parse_def(signature))
+	local arg_err = ("bad argument " .. n_to_exp_got):gsub("?", s.name)
+	local ret_err = ("bad returning value " .. n_to_exp_got):gsub("?", s.name)
+
+	return function(...)
+		wrap_return(arg_err, check(s.param_types, ...))
+		return wrap_return(ret_err, check(s.return_types, f(...)))
+	end
+end
+
+return typecheck
