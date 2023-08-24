d84f35cca3b9c09e40d0086a1e4e8987cfea3eb3
semyon422 <semyon422@users.noreply.github.com>
2023-08-24 18:48:21 +0500

    path_util.eval_path

--- path_util.lua
+++ path_util.lua
@@ -7,4 +7,10 @@ function path_util.fix_illegal(s, c)
 	return (s:gsub('[/\\?%*:|"<>]', c or "_"))
 end
 
+---@param path string
+---@return string
+function path_util.eval_path(path)
+	return (path:gsub("\\", "/"):gsub("/[^/]-/%.%./", "/"))
+end
+
 return path_util
