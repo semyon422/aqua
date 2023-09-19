44bcc4f2233cc22770659f4c0238eeaf0952ea20
semyon422 <semyon422@users.noreply.github.com>
2023-09-19 14:58:06 +0500

    Create sleep.lua

---
+++ sleep.lua
@@ -0,0 +1,26 @@
+local ffi = require("ffi")
+local jit = require("jit")
+
+if jit.os ~= "Linux" then
+	return love.timer.sleep
+end
+
+ffi.cdef[[
+struct timespec {
+	long tv_sec;
+	long tv_nsec;
+};
+int nanosleep(const struct timespec *__requested_time, struct timespec *__remaining);
+]]
+
+local rt = ffi.new("struct timespec[1]")
+
+---@param s number
+local function nanosleep(s)
+	local i, f = math.modf(s)
+	rt[0].tv_sec = i
+	rt[0].tv_nsec = f * 1e9
+	ffi.C.nanosleep(rt, nil)
+end
+
+return nanosleep
