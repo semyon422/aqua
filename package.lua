dea0296d2f44a03feecf59b030f5d4b88cc1097b
semyon422 <semyon422@users.noreply.github.com>
2023-10-20 13:06:21 +0500

    Update package.lua

--- package.lua
+++ package.lua
@@ -1,30 +1,36 @@
-local requirePath = "?.lua;?/init.lua"
-local cRequirePath = "./?.so"
-
-local _package = package
+local lua_package = package
 local package = {}
 
-local lfs = love.filesystem
+local lfs = love and love.filesystem
 
 function package.reset()
-	lfs.setRequirePath(requirePath)
-	lfs.setCRequirePath(cRequirePath)
-	package.path = requirePath
-	package.cpath = cRequirePath
+	if lfs then
+		lfs.setRequirePath("")
+		lfs.setCRequirePath("")
+	end
+	lua_package.path = ""
+	lua_package.cpath = ""
+	package.add(".")
 end
 
 ---@param path string
 function package.add(path)
-	lfs.setRequirePath(lfs.getRequirePath() .. (";path/?.lua;path/?/init.lua"):gsub("path", path))
-	_package.path = lfs.getRequirePath()
+	local added = (";path/?.lua;path/?/init.lua"):gsub("path", path)
+	if lfs then
+		lfs.setRequirePath(lua_package.path .. added)
+	end
+	lua_package.path = lua_package.path .. added
 end
 
 local ext = jit.os == "Windows" and "dll" or "so"
 
 ---@param path string
 function package.addc(path)
-	lfs.setCRequirePath(lfs.getCRequirePath() .. (";path/?." .. ext):gsub("path", path))
-	_package.cpath = lfs.getCRequirePath()
+	local added = (";path/?." .. ext):gsub("path", path)
+	if lfs then
+		lfs.setCRequirePath(lua_package.cpath .. added)
+	end
+	lua_package.cpath = lua_package.cpath .. added
 end
 
 return package
