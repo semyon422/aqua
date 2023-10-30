2145d9ad10eb1ad4f184914b1853a9be151db7d7
semyon422 <semyon422@users.noreply.github.com>
2023-10-30 22:33:39 +0500

    Fix handling usecases with missing repos

--- http/UsecaseViewHandler.lua
+++ http/UsecaseViewHandler.lua
@@ -9,9 +9,17 @@ function UsecaseViewHandler:new(usecases, usecase_repos, default_results, views)
 	self.views = views
 end
 
+function UsecaseViewHandler:getUsecaseRepos(usecase_name)
+	local repos = self.usecase_repos[usecase_name]
+	if not repos then
+		return
+	end
+	return unpack(repos)
+end
+
 function UsecaseViewHandler:handle_params(params, usecase_name, results)
 	local usecase = self.usecases[usecase_name]
-	local result_type, result = usecase(params, unpack(self.usecase_repos[usecase_name]))
+	local result_type, result = usecase(params, self:getUsecaseRepos(usecase_name))
 	local code_view = results[result_type] or self.default_results[result_type]
 	local code, view_name = code_view[1], code_view[2]
 	local res_body, headers = self.views[view_name](result)
