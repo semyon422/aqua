53c65276b206e00c01e2fba0187afd906a25547b
semyon422 <semyon422@users.noreply.github.com>
2023-10-31 17:04:36 +0500

    Usecase is object

--- http/UsecaseViewHandler.lua
+++ http/UsecaseViewHandler.lua
@@ -19,7 +19,7 @@ end
 
 function UsecaseViewHandler:handle_params(params, usecase_name, results)
 	local usecase = self.usecases[usecase_name]
-	local result_type, result = usecase(params, self:getUsecaseRepos(usecase_name))
+	local result_type, result = usecase:run(params, self:getUsecaseRepos(usecase_name))
 	local code_view = results[result_type] or self.default_results[result_type]
 	local code, view_name = code_view[1], code_view[2]
 	local res_body, headers = self.views[view_name](result)
