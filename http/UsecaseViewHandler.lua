3b8adbcfc112dc72d62994ff632e25816dc82255
semyon422 <semyon422@users.noreply.github.com>
2023-11-01 17:10:57 +0500

    Add params for views

--- http/UsecaseViewHandler.lua
+++ http/UsecaseViewHandler.lua
@@ -13,8 +13,8 @@ function UsecaseViewHandler:handle_params(params, usecase_name, results)
 	local usecase = self.usecases[usecase_name]
 	local result_type, result = usecase:run(params, self.models)
 	local code_view = results[result_type] or self.default_results[result_type]
-	local code, view_name = code_view[1], code_view[2]
-	local res_body, headers = self.views[view_name](result)
+	local code, view_name, view_params = unpack(code_view)
+	local res_body, headers = self.views[view_name](result, view_params)
 	return code, headers, res_body
 end
 
