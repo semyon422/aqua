955c68dad6fb65a135eaac8890d55fd2736025f3
semyon422 <semyon422@users.noreply.github.com>
2023-11-03 21:18:09 +0500

    Set headers in results instead of views

--- http/UsecaseViewHandler.lua
+++ http/UsecaseViewHandler.lua
@@ -12,10 +12,16 @@ end
 function UsecaseViewHandler:handle_params(params, usecase_name, results)
 	local usecase = self.usecases[usecase_name]
 	local result_type, result = usecase:run(params, self.models)
-	local code_view = results[result_type] or self.default_results[result_type]
-	local code, view_name, view_params = unpack(code_view)
-	local res_body, headers = self.views[view_name](result, view_params)
-	return code, headers, res_body
+
+	local code_view_headers = results[result_type] or self.default_results[result_type]
+	local code, view_name, headers = unpack(code_view_headers)
+
+	local res_body
+	if view_name then
+		res_body = self.views[view_name](result)
+	end
+
+	return code or 200, headers or {}, res_body or ""
 end
 
 return UsecaseViewHandler
