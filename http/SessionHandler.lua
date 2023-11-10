b844cb574f84b7fa0bf2e7c1faaf13f4e786cee2
semyon422 <semyon422@users.noreply.github.com>
2023-11-10 18:39:12 +0500

    Remove params.cookies from params

--- http/SessionHandler.lua
+++ http/SessionHandler.lua
@@ -11,20 +11,20 @@ function SessionHandler:new(session_config)
 end
 
 function SessionHandler:decode(params, headers)
-	params.cookies = cookie_util.decode(headers["Cookie"])
+	local cookies = cookie_util.decode(headers["Cookie"])
 	params.session = session_util.decode(
-		params.cookies[self.session_config.name],
+		cookies[self.session_config.name],
 		self.session_config.secret
 	) or {}
 end
 
 function SessionHandler:encode(params, headers)
-	params.cookies = {}
-	params.cookies[self.session_config.name] = session_util.encode(
+	local cookies = {}
+	cookies[self.session_config.name] = session_util.encode(
 		params.session,
 		self.session_config.secret
 	)
-	headers["Set-Cookie"] = cookie_util.encode(params.cookies)
+	headers["Set-Cookie"] = cookie_util.encode(cookies)
 end
 
 return SessionHandler
