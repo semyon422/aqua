107cb1ab2a14e9f987f5939b9eaeb479cd815d7a
semyon422 <semyon422@users.noreply.github.com>
2023-12-12 20:31:10 +0500

    Add path_util.join

--- path_util.lua
+++ path_util.lua
@@ -13,4 +13,14 @@ function path_util.eval_path(path)
 	return (path:gsub("\\", "/"):gsub("/[^/]-/%.%./", "/"))
 end
 
+---@param ... string?
+---@return string
+function path_util.join(...)
+	local t = {}
+	for i = 1, select("#", ...) do
+		table.insert(t, (select(i, ...)))
+	end
+	return table.concat(t, "/")
+end
+
 return path_util
