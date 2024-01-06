adb7ecd97e32f06ea5f233bcd51162f1b0a84014
semyon422 <semyon422@users.noreply.github.com>
2024-01-06 21:45:45 +0500

    Add annotations in Access class

--- abac/Access.lua
+++ abac/Access.lua
@@ -1,6 +1,8 @@
 local class = require("class")
 local PolicySet = require("abac.PolicySet")
 
+---@class abac.Access
+---@operator call: abac.Access
 local Access = class()
 
 ---@param rules_repo table
@@ -8,6 +10,10 @@ function Access:new(rules_repo)
 	self.rules_repo = rules_repo
 end
 
+---@param params table
+---@param policy_set_config table
+---@return boolean
+---@return string?
 function Access:authorize(params, policy_set_config)
 	local policy_set = PolicySet(policy_set_config)
 	local dec, err = policy_set:evaluate(self.rules_repo, params)
