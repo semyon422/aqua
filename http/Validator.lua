0d9eaa9cf622548d4127f000fd2583817bd46b92
semyon422 <semyon422@users.noreply.github.com>
2024-01-11 20:46:59 +0500

    Update Validator.lua

--- http/Validator.lua
+++ http/Validator.lua
@@ -1,9 +1,10 @@
 local class = require("class")
+local validation = require("validation")
 
 local Validator = class()
 
-function Validator:validate(params, validate_config)
-	return true
+function Validator:validate(params, schema)
+	return validation.validate(params, schema)
 end
 
 return Validator
