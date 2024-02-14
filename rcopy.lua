c3ba79bb8b43ff1698cacb4d6adffa009b7cb012
semyon422 <semyon422@users.noreply.github.com>
2024-02-14 16:09:40 +0500

    Use asserts in rcopy

--- rcopy.lua
+++ rcopy.lua
@@ -12,14 +12,8 @@ local function rcopy(from, to)
 		if love.filesystem.getInfo(fileFrom, "directory") then
 			rcopy(fileFrom, fileTo)
 		else
-			local content, err = love.filesystem.read(fileFrom)
-			if not content then
-				print(err)
-			end
-			local ok, err = love.filesystem.write(fileTo, content)
-			if not ok then
-				print(err)
-			end
+			local content = assert(love.filesystem.read(fileFrom))
+			assert(love.filesystem.write(fileTo, content))
 		end
 	end
 end
