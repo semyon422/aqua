80d35528c0edf4d244b2c01125a37aa84fa03b1a
semyon422 <semyon422@users.noreply.github.com>
2024-07-02 20:35:51 +0500

    Create autoread.lua

---
+++ autoread.lua
@@ -0,0 +1,16 @@
+local mt = {}
+
+---@param t table
+---@param k string
+---@return any?
+function mt.__index(t, k)
+	local f = assert(io.open(t.__path:format(k)))
+	local s = f:read("*a")
+	f:close()
+	t[k] = s
+	return s
+end
+
+return function(path)
+	return setmetatable({__path = path}, mt)
+end
