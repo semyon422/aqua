23ecccc1232cf390cc745d12f94b34f7de4ef222
semyon422 <semyon422@users.noreply.github.com>
2024-09-26 23:05:55 +0500

    Fix symlinks in ls.lua

--- ls.lua
+++ ls.lua
@@ -1,7 +1,15 @@
 local ls = {}
 
+--[[
+	-p  append / indicator to directories
+	-A  do not list implied . and ..
+	-L  when showing file information for a symbolic
+        link, show information for the file the link
+        references rather than for the link itself
+]]
+
 function ls.iter(path)
-	local cmd = "ls -pA"
+	local cmd = "ls -pAL"
 	if path and #path > 0 then
 		cmd = cmd .. " %q"
 	end
