c6e41784aec02bd82acf8738978f1bba4f9cc05f
semyon422 <semyon422@users.noreply.github.com>
2024-10-09 00:03:26 +0500

    Use list of results in FakeSocket

--- web/socket/FakeSocket.lua
+++ web/socket/FakeSocket.lua
@@ -4,21 +4,19 @@ local ISocket = require("web.socket.ISocket")
 ---@operator call: web.FakeSocket
 local FakeSocket = ISocket + {}
 
----@param res string|integer?
----@param state "closed"|"timeout"?
-function FakeSocket:new(res, state)
-	self.res = res
-	self.state = state
+---@param results {[1]: string|integer, [2]: "closed"|"timeout"}[]
+function FakeSocket:new(results)
+	self.results = results
 end
 
----@param p integer
+---@param pattern "*a"|"*l"|integer?
+---@param prefix string?
 ---@return string?
 ---@return "closed"|"timeout"?
 ---@return string?
-function FakeSocket:receive(p)
-	local state = self.state
-	local res = self.res
-	---@cast res string
+function FakeSocket:receive(pattern, prefix)
+	---@type string, "closed"|"timeout"
+	local res, state = unpack(table.remove(self.results, 1))
 	if state == nil then
 		return res
 	elseif state == "timeout" then
@@ -36,9 +34,8 @@ end
 ---@return "closed"|"timeout"?
 ---@return integer?
 function FakeSocket:send(data, i, j)
-	local state = self.state
-	local res = self.res
-	---@cast res integer
+	---@type integer, "closed"|"timeout"
+	local res, state = unpack(table.remove(self.results, 1))
 	if state == nil then
 		return res
 	elseif state == "timeout" then
