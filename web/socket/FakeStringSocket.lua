4f4eb708026e264695b15ee04dd0c0094db7bce9
semyon422 <semyon422@users.noreply.github.com>
2024-10-10 23:23:23 +0500

    Impl FakeStringSocket:send()

--- web/socket/FakeStringSocket.lua
+++ web/socket/FakeStringSocket.lua
@@ -4,9 +4,17 @@ local ISocket = require("web.socket.ISocket")
 ---@operator call: web.FakeStringSocket
 local FakeStringSocket = ISocket + {}
 
----@param data string
-function FakeStringSocket:new(data)
-	self.remainder = data
+---@param data string?
+---@param max_size integer?
+function FakeStringSocket:new(data, max_size)
+	self.remainder = data or ""
+	self.max_size = max_size or math.huge
+end
+
+---@return 1
+function FakeStringSocket:close()
+	self.closed = true
+	return 1
 end
 
 ---@param size integer?
@@ -34,7 +42,37 @@ function FakeStringSocket:receive(size, prefix)
 		return ret
 	end
 
-	return nil, "closed", ret
+	local err = self.closed and "closed" or "timeout"
+
+	return nil, err, ret
+end
+
+---@param data string
+---@param i integer?
+---@param j integer?
+---@return integer?
+---@return "closed"|"timeout"?
+---@return integer?
+function FakeStringSocket:send(data, i, j)
+	if self.closed then
+		return nil, "closed", 0
+	end
+
+	i = i or 1
+	j = j or #data
+
+	local data_size = j - i + 1
+	local avail_size = self.max_size - #self.remainder
+
+	if avail_size >= data_size then
+		self.remainder = self.remainder .. data:sub(i, j)
+		return j
+	end
+
+	local last_byte = i + avail_size - 1
+	self.remainder = self.remainder .. data:sub(i, last_byte)
+
+	return nil, "timeout", last_byte
 end
 
 return FakeStringSocket
--- web/socket/FakeStringSocket_test.lua
+++ web/socket/FakeStringSocket_test.lua
@@ -3,23 +3,80 @@ local FakeStringSocket = require("web.socket.FakeStringSocket")
 local test = {}
 
 ---@param t testing.T
-function test.receive_exact(t)
-	local soc = FakeStringSocket("qwerty")
+function test.receive_size_exact(t)
+	local soc = FakeStringSocket()
+
+	soc:send("qwe")
+	soc:close()
 
 	t:tdeq({soc:receive(3)}, {"qwe"})
-	t:tdeq({soc:receive(3)}, {"rty"})
 	t:tdeq({soc:receive(100)}, {nil, "closed", ""})
 	t:tdeq({soc:receive(100)}, {nil, "closed", ""})
 end
 
 ---@param t testing.T
 function test.receive_size_more(t)
-	local soc = FakeStringSocket("qwerty")
+	local soc = FakeStringSocket()
 
-	t:tdeq({soc:receive(3)}, {"qwe"})
-	t:tdeq({soc:receive(4)}, {nil, "closed", "rty"})
+	soc:send("qwe")
+	soc:close()
+
+	t:tdeq({soc:receive(4)}, {nil, "closed", "qwe"})
 	t:tdeq({soc:receive(100)}, {nil, "closed", ""})
 	t:tdeq({soc:receive(100)}, {nil, "closed", ""})
+end
+
+---@param t testing.T
+function test.receive_size_exact_timeout(t)
+	local soc = FakeStringSocket()
+
+	soc:send("qwe")
+
+	t:tdeq({soc:receive(4)}, {nil, "timeout", "qwe"})
+
+	soc:send("rty")
+	soc:close()
+
+	t:tdeq({soc:receive(3)}, {"rty"})
+	t:tdeq({soc:receive(100)}, {nil, "closed", ""})
+	t:tdeq({soc:receive(100)}, {nil, "closed", ""})
+end
+
+---@param t testing.T
+function test.send_size_closed(t)
+	local soc = FakeStringSocket(nil, 6)
+
+	t:tdeq({soc:send("qwerty", 1, 2)}, {2})
+
+	soc:close()
+
+	t:tdeq({soc:send("qwerty", 3, 4)}, {nil, "closed", 0})
+	t:tdeq({soc:receive(100)}, {nil, "closed", "qw"})
+	t:tdeq({soc:receive(100)}, {nil, "closed", ""})
+	t:tdeq({soc:receive(100)}, {nil, "closed", ""})
+end
+
+---@param t testing.T
+function test.send_size_exact(t)
+	local soc = FakeStringSocket(nil, 6)
+
+	t:tdeq({soc:send("qwerty", 1, 2)}, {2})
+	t:tdeq({soc:send("qwerty", 3, 4)}, {4})
+	t:tdeq({soc:send("qwerty", 5, 6)}, {6})
+	t:tdeq({soc:receive(100)}, {nil, "timeout", "qwerty"})
+	t:tdeq({soc:receive(100)}, {nil, "timeout", ""})
+	t:tdeq({soc:receive(100)}, {nil, "timeout", ""})
+end
+
+---@param t testing.T
+function test.send_size_more(t)
+	local soc = FakeStringSocket(nil, 4)
+
+	t:tdeq({soc:send("qwerty", 1, 2)}, {2})
+	t:tdeq({soc:send("qwerty", 3, 6)}, {nil, "timeout", 4})
+	t:tdeq({soc:receive(100)}, {nil, "timeout", "qwer"})
+	t:tdeq({soc:receive(100)}, {nil, "timeout", ""})
+	t:tdeq({soc:receive(100)}, {nil, "timeout", ""})
 end
 
 return test
