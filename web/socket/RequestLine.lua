887697a554750cb47f6634ad3c944e851b881950
semyon422 <semyon422@users.noreply.github.com>
2024-10-14 23:33:31 +0500

    Add RequestLine and StatusLine classes

---
+++ web/socket/RequestLine.lua
@@ -0,0 +1,47 @@
+local class = require("class")
+
+-- https://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html
+
+---@class web.RequestLine
+---@operator call: web.RequestLine
+---@field method string
+---@field uri string
+---@field version string
+local RequestLine = class()
+
+RequestLine.method = "GET"
+RequestLine.uri = "/"
+RequestLine.version = "HTTP/1.1"
+
+---@param method string
+---@param uri string
+function RequestLine:new(method, uri)
+	self.method = method
+	self.uri = uri
+end
+
+---@param soc web.AsyncSocket
+---@return web.RequestLine?
+---@return "closed"?
+---@return string?
+function RequestLine:receive(soc)
+	local data, err, partial = soc:receive("*l")
+	if not data then
+		return nil, err, partial
+	end
+
+	self.method, self.uri, self.version = data:match("^(%S+)%s+(%S+)%s+(%S+)")
+
+	return self
+end
+
+---@param soc web.AsyncSocket
+---@return integer?
+---@return "closed"?
+---@return integer?
+function RequestLine:send(soc)
+	local status_line = ("%s %s %s\r\n"):format(self.method, self.uri, self.version)
+	return soc:send(status_line)
+end
+
+return RequestLine
---
+++ web/socket/StatusLine.lua
@@ -0,0 +1,47 @@
+local class = require("class")
+local codes = require("web.socket.codes")
+
+-- https://www.w3.org/Protocols/rfc2616/rfc2616-sec6.html
+
+---@class web.StatusLine
+---@operator call: web.StatusLine
+---@field version string
+---@field status string|integer
+---@field reason string
+local StatusLine = class()
+
+StatusLine.version = "HTTP/1.1"
+StatusLine.status = 200
+
+---@param status string|integer
+---@param reason string
+function StatusLine:new(status, reason)
+	self.status = status
+	self.reason = reason
+end
+
+---@param soc web.AsyncSocket
+---@return web.StatusLine?
+---@return "closed"?
+---@return string?
+function StatusLine:receive(soc)
+	local data, err, partial = soc:receive("*l")
+	if not data then
+		return nil, err, partial
+	end
+
+	self.version, self.status, self.reason = data:match("^(%S+)%s+(%S+)%s+(%S+)")
+
+	return self
+end
+
+---@param soc web.AsyncSocket
+---@return integer?
+---@return "closed"?
+---@return integer?
+function StatusLine:send(soc)
+	local status_line = ("%s %s %s\r\n"):format(self.version, self.status, self.reason or codes[self.status])
+	return soc:send(status_line)
+end
+
+return StatusLine
