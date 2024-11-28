967b8d25f6f4c723b02f15cfd67fea4db2a4c483
semyon422 <semyon422@users.noreply.github.com>
2024-11-28 21:11:43 +0500

    Update Headers_test.lua

--- web/http/Headers_test.lua
+++ web/http/Headers_test.lua
@@ -5,7 +5,25 @@ local ExtendedSocket = require("web.socket.ExtendedSocket")
 local test = {}
 
 ---@param t testing.T
-function test.basic(t)
+function test.add_set_get(t)
+	local headers = Headers()
+	headers:add("Name1", "value1")
+	headers:add("Name1", "value2")
+	headers:add("Name2", "value2")
+
+	t:tdeq({headers:get("Name1")}, {"value1", "value2"})
+	t:tdeq({headers:get("name1")}, {"value1", "value2"})  -- lowercase
+	t:tdeq({headers:get("Name2")}, {"value2"})
+
+	headers:set("Name1", "value3")
+	headers:set("Name2", {"value3"})
+
+	t:tdeq({headers:get("Name1")}, {"value3"})
+	t:tdeq({headers:get("Name2")}, {"value3"})
+end
+
+---@param t testing.T
+function test.multiple(t)
 	local str_soc = StringSocket()
 	local soc = ExtendedSocket(str_soc)
 
@@ -20,7 +38,6 @@ function test.basic(t)
 	headers = Headers(soc)
 	t:tdeq({headers:receive()}, {true})
 	t:tdeq({headers:get("Name1")}, {"value1", "value2"})
-	t:tdeq({headers:get("name1")}, {"value1", "value2"})
 	t:tdeq({headers:get("Name2")}, {"value2"})
 end
 
@@ -36,6 +53,78 @@ function test.empty(t)
 
 	headers = Headers(soc)
 	t:tdeq({headers:receive()}, {true})
+	t:tdeq(headers.headers, {})
+end
+
+---@param t testing.T
+function test.folded_space(t)
+	local str_soc = StringSocket()
+	local soc = ExtendedSocket(str_soc)
+
+	str_soc:send("Name: value1\r\n value2\r\n\r\n")
+
+	local headers = Headers(soc)
+	t:tdeq({headers:receive()}, {true})
+	t:tdeq(headers.headers, {name = {"value1 value2"}})
+end
+
+---@param t testing.T
+function test.folded_tab(t)
+	local str_soc = StringSocket()
+	local soc = ExtendedSocket(str_soc)
+
+	str_soc:send("Name: value1\r\n\tvalue2\r\n\r\n")
+
+	local headers = Headers(soc)
+	t:tdeq({headers:receive()}, {true})
+	t:tdeq(headers.headers, {name = {"value1\tvalue2"}})
+end
+
+---@param t testing.T
+function test.timeout(t)
+	local str_soc = StringSocket()
+	local soc = ExtendedSocket(str_soc)
+
+	str_soc:send("Name: value")
+
+	local headers = Headers(soc)
+	t:tdeq({headers:receive()}, {nil, "timeout"})
+	t:tdeq(headers.headers, {})
+end
+
+---@param t testing.T
+function test.malformed(t)
+	local str_soc = StringSocket()
+	local soc = ExtendedSocket(str_soc)
+
+	str_soc:send("Name value\r\n\r\n")
+
+	local headers = Headers(soc)
+	t:tdeq({headers:receive()}, {nil, "malformed headers"})
+	t:tdeq(headers.headers, {})
+end
+
+---@param t testing.T
+function test.no_end_line_timeout(t)
+	local str_soc = StringSocket()
+	local soc = ExtendedSocket(str_soc)
+
+	str_soc:send("Name: value\r\n")
+
+	local headers = Headers(soc)
+	t:tdeq({headers:receive()}, {nil, "timeout"})
+	t:tdeq(headers.headers, {})
+end
+
+---@param t testing.T
+function test.no_end_line_folded_timeout(t)
+	local str_soc = StringSocket()
+	local soc = ExtendedSocket(str_soc)
+
+	str_soc:send("Name: value1\r\n value2\r\n")
+
+	local headers = Headers(soc)
+	t:tdeq({headers:receive()}, {nil, "timeout"})
 	t:tdeq(headers.headers, {})
 end
 
