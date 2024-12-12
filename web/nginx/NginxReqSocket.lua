1a6bacdd6dd82957d40a6170d3d35159e3ef2aba
semyon422 <semyon422@users.noreply.github.com>
2024-12-12 21:57:28 +0500

    Add NginxTcpSocket

--- web/nginx/NginxReqSocket.lua
+++ web/nginx/NginxReqSocket.lua
@@ -1,50 +1,11 @@
-local IExtendedSocket = require("web.socket.IExtendedSocket")
+local NginxTcpSocket = require("web.nginx.NginxTcpSocket")
 
----@class web.NginxReqSocket: web.IExtendedSocket
+---@class web.NginxReqSocket: web.NginxTcpSocket
 ---@operator call: web.NginxReqSocket
-local NginxReqSocket = IExtendedSocket + {}
+local NginxReqSocket = NginxTcpSocket + {}
 
 function NginxReqSocket:new()
 	self.soc = assert(ngx.req.socket(true))
-end
-
----@param pattern "*a"|"*l"|integer?
----@param prefix string?
----@return string?
----@return "closed"|"timeout"?
----@return string?
-function NginxReqSocket:receive(pattern, prefix)
-	return self.soc:receive(pattern)
-end
-
----@param max integer
----@return string?
----@return "closed"|"timeout"?
-function NginxReqSocket:receiveany(max)
-	return self.soc:receiveany(max)
-end
-
----@param pattern string
----@param options {inclusive: boolean?}?
----@return fun(size: integer?): string?, "closed"|"timeout"?, string?
-function NginxReqSocket:receiveuntil(pattern, options)
-	return self.soc:receiveuntil(pattern, options)
-end
-
----@param data string
----@param i integer?
----@param j integer?
----@return integer?
----@return "closed"|"timeout"?
----@return integer?
-function NginxReqSocket:send(data, i, j)
-	i, j = self:normalize_bounds(data, i, j)
-	return self.soc:send(data:sub(i, j))
-end
-
----@return 1
-function NginxReqSocket:close()
-	return self.soc:close()  ---@diagnostic disable-line
 end
 
 return NginxReqSocket
---
+++ web/nginx/NginxTcpSocket.lua
@@ -0,0 +1,70 @@
+local ITcpSocket = require("web.socket.ITcpSocket")
+
+---@class web.NginxTcpSocket: web.ITcpSocket
+---@operator call: web.NginxTcpSocket
+local NginxTcpSocket = ITcpSocket + {}
+
+---@param soc tcpsock?
+function NginxTcpSocket:new(soc)
+	self.soc = soc or assert(ngx.socket.tcp())
+end
+
+---@param host string
+---@param port integer
+---@return 1?
+---@return string?
+function NginxTcpSocket:connect(host, port)
+	return self.soc:connect(host, port)  ---@diagnostic disable-line
+end
+
+---@return 1?
+---@return string?
+function NginxTcpSocket:sslhandshake()
+	return self.soc:sslhandshake()  ---@diagnostic disable-line
+end
+
+---@param time integer
+function NginxTcpSocket:settimeout(time)
+	return self.soc:settimeout(time * 1000)  ---@diagnostic disable-line
+end
+
+---@param pattern "*a"|"*l"|integer?
+---@param prefix string?
+---@return string?
+---@return "closed"|"timeout"?
+---@return string?
+function NginxTcpSocket:receive(pattern, prefix)
+	return self.soc:receive(pattern)
+end
+
+---@param max integer
+---@return string?
+---@return "closed"|"timeout"?
+function NginxTcpSocket:receiveany(max)
+	return self.soc:receiveany(max)
+end
+
+---@param pattern string
+---@param options {inclusive: boolean?}?
+---@return fun(size: integer?): string?, "closed"|"timeout"?, string?
+function NginxTcpSocket:receiveuntil(pattern, options)
+	return self.soc:receiveuntil(pattern, options)
+end
+
+---@param data string
+---@param i integer?
+---@param j integer?
+---@return integer?
+---@return "closed"|"timeout"?
+---@return integer?
+function NginxTcpSocket:send(data, i, j)
+	i, j = self:normalize_bounds(data, i, j)
+	return self.soc:send(data:sub(i, j))
+end
+
+---@return 1
+function NginxTcpSocket:close()
+	return self.soc:close()  ---@diagnostic disable-line
+end
+
+return NginxTcpSocket
