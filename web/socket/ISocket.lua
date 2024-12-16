fdc80d795c5c3c78d8f7d351e69856d23b4f4533
semyon422 <semyon422@users.noreply.github.com>
2024-12-16 21:08:18 +0500

    Update ISocket.lua

--- web/socket/ISocket.lua
+++ web/socket/ISocket.lua
@@ -26,28 +26,6 @@ function ISocket:send(data, i, j)
 	error("not implemented")
 end
 
----@param data string
----@param i integer?
----@param j integer?
----@return integer
----@return integer
-function ISocket:normalize_bounds(data, i, j)
-	i = i or 1
-	j = j or #data
-
-	if i < 0 then
-		i = i + #data + 1
-	end
-	if j < 0 then
-		j = j + #data + 1
-	end
-
-	i = math.max(i, 1)
-	j = math.max(math.min(j, #data), i - 1)
-
-	return i, j
-end
-
 ---@param size integer
 ---@return string?
 ---@return "closed"|"timeout"?
