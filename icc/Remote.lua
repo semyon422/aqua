cb1a95318b8b896881444c69458e11d56e6499a1
semyon422 <semyon422@users.noreply.github.com>
2025-01-17 23:50:20 +0500

    Refactor icc.Remote

--- icc/Remote.lua
+++ icc/Remote.lua
@@ -1,6 +1,8 @@
 local class = require("class")
 local table_util = require("table_util")
 
+---@alias icc.Key string|number
+
 ---@class icc.Remote
 ---@operator call: icc.Remote
 ---@field [string] icc.Remote
@@ -8,31 +10,47 @@ local Remote = class()
 
 ---@param th icc.TaskHandler
 ---@param peer icc.IPeer
----@param path string[]?
+---@param path icc.Key[]?
 function Remote:new(th, peer, path)
 	self.__th = th
 	self.__peer = peer
 	self.__path = path or {}
 end
 
----@param k string|number
+---@param r icc.Remote
+---@return icc.TaskHandler
+---@return icc.IPeer
+---@return icc.Key[]
+local function unpack_remote(r)
+	local th = r.__th ---@cast th icc.TaskHandler
+	local peer = r.__peer ---@cast peer icc.IPeer
+	local path = r.__path ---@cast path icc.Key[]
+	return th, peer, path
+end
+
+---@param path icc.Key[]
+---@param k icc.Key
+---@return icc.Key[]
+local function copy_append(path, k)
+	local _path = table_util.copy(path)
+	---@cast _path icc.Key[]
+	table.insert(_path, k)
+	return _path
+end
+
+---@param k icc.Key
 ---@return icc.Remote
 function Remote:__index(k)
-	local path = table_util.copy(self.__path)
-	table.insert(path, k)
-	return Remote(self.__th, self.__peer, path)
+	local th, peer, path = unpack_remote(self)
+	return Remote(th, peer, copy_append(path, k))
 end
 
 ---@param ... any
 ---@return any ...
 function Remote:__call(...)
 	local is_method = getmetatable(...) == Remote
-	return self.__th:call(
-		self.__peer,
-		self.__path,
-		is_method,
-		select(is_method and 2 or 1, ...)
-	)
+	local th, peer, path = unpack_remote(self)
+	return th:call(peer, path, is_method, select(is_method and 2 or 1, ...))
 end
 
 return Remote
