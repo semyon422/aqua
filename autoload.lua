208690954449550d6f836c25d4a38f845fd7d82b
semyon422 <semyon422@users.noreply.github.com>
2025-02-18 23:04:13 +0500

    Safe mode for autoload

--- autoload.lua
+++ autoload.lua
@@ -1,14 +1,28 @@
 local mt = {}
 
----@param t table
----@param mod_name string
+---@alias util.Autoload {[1]: string, [2]: boolean, [string]: any}
+
+---@param t util.Autoload
+---@param k string
 ---@return any?
-function mt.__index(t, mod_name)
-	local mod = require(t[1] .. "." .. mod_name)
-	t[mod_name] = mod
+function mt.__index(t, k)
+	local prefix, safe = t[1], t[2]
+	---@type boolean, any
+	local ok, mod = pcall(require, prefix .. "." .. k)
+	if not safe then
+		assert(ok, mod)
+	elseif not ok then
+		mod = true -- behave like empty file
+	end
+	t[k] = mod
 	return mod
 end
 
-return function(prefix)
-	return setmetatable({prefix}, mt)
+---@param prefix string
+---@param safe boolean?
+---@return util.Autoload
+local function autoload(prefix, safe)
+	return setmetatable({prefix, not not safe}, mt)
 end
+
+return autoload
