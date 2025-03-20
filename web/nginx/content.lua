2db9d7b2a5387ce2fb74b890f829b50b0990c236
semyon422 <semyon422@users.noreply.github.com>
2025-03-20 22:49:34 +0500

    Pass client IP to app handler

--- web/nginx/content.lua
+++ web/nginx/content.lua
@@ -2,12 +2,25 @@ local NginxRequest = require("web.nginx.NginxRequest")
 local NginxReqSocket = require("web.nginx.NginxReqSocket")
 local Response = require("web.http.Response")
 
----@param handle fun(req: web.IRequest, res: web.IResponse)
-local function run(handle)
+---@type web.NginxConfig
+local config = require("nginx_config")
+
+local function run()
+	---@type fun(req: web.IRequest, res: web.IResponse, ip: string)
+	local handle = require(config.handler)
+
 	local soc = NginxReqSocket()
 	local req = NginxRequest(soc)
 	local res = Response(soc, "w")
-	return handle(req, res)
+
+	req:receive_headers()
+
+	local ip = soc:getpeername()
+	if config.proxied then
+		ip = assert(req.headers:get("X-Real-IP"), "missing real ip")
+	end
+
+	return handle(req, res, ip)
 end
 
 return run
--- web/nginx/nginx_config.lua
+++ web/nginx/nginx_config.lua
@@ -1,9 +1,10 @@
 ---@class web.NginxConfig
 local config = {
-	listen = 8180,
+	listen = 8080,
 	lua_code_cache = "off",
 	client_max_body_size = "10M",
 	handler = "sea.app.handler",
+	proxied = false,
 	require = {
 		"socket",
 		"ltn12",
