fdb8bf677b7f7fdfd154ab7f5f93d53970e83445
semyon422 <semyon422@users.noreply.github.com>
2025-03-21 22:23:30 +0500

    Add layout in Views, impl Views:render_send()

--- web/framework/page/Views.lua
+++ web/framework/page/Views.lua
@@ -7,8 +7,10 @@ local class = require("class")
 local Views = class()
 
 ---@param templates {[string]: fun(env: table): string}
-function Views:new(templates)
+---@param layout string?
+function Views:new(templates, layout)
 	self.templates = templates
+	self.layout = layout
 end
 
 ---@param env table
@@ -31,27 +33,43 @@ function Views:template(path)
 end
 
 ---@param view_config table|string
----@param result table
+---@param ctx table
+---@param add_layout boolean?
 ---@return string
-function Views:render(view_config, result)
+function Views:render(view_config, ctx, add_layout)
+	if add_layout and self.layout then
+		view_config = {[self.layout] = view_config}
+	end
+
 	if type(view_config) == "string" then
-		return self:template(view_config)(result)
+		return self:template(view_config)(ctx)
 	end
 
 	if not view_config[1] then
 		local outer, inner = next(view_config)
 		---@cast outer string
 		---@cast inner table|string
-		result.inner = self:render(inner, result)
-		return self:render(outer, result)
+		ctx.inner = self:render(inner, ctx)
+		return self:render(outer, ctx)
 	end
 
 	local out = {}
 	for _, vc in ipairs(view_config) do
-		local s = self:render(vc, result)
+		local s = self:render(vc, ctx)
 		table.insert(out, s)
 	end
 	return table.concat(out)
+end
+
+---@param res web.IResponse
+---@param view_config table|string
+---@param ctx table
+---@param add_layout boolean?
+function Views:render_send(res, view_config, ctx, add_layout)
+	local s = self:render(view_config, ctx, add_layout)
+	res.headers:set("Content-Type", "text/html")
+	res:set_length(#s)
+	res:send(s)
 end
 
 return Views
