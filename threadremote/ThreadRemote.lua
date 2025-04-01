a4cd527e7864a3f05b8c22bf72de8d70a175e826
semyon422 <semyon422@users.noreply.github.com>
2025-04-01 21:17:45 +0500

    Mostly complete threadremote module

--- threadremote/ThreadRemote.lua
+++ threadremote/ThreadRemote.lua
@@ -1,8 +1,25 @@
 local class = require("class")
+local table_util = require("table_util")
 local Remote = require("icc.Remote")
 local TaskHandler = require("icc.TaskHandler")
 local RemoteHandler = require("icc.RemoteHandler")
-local LoveThreadPeer = require("icc.LoveThreadPeer")
+local Message = require("icc.Message")
+
+---@type string
+local codestring
+
+---@param id any
+---@return string
+local function getCodeString(id)
+	if not codestring then
+		local path = "aqua/threadremote/threadcode.lua"
+		codestring = love.filesystem.read(path)
+	end
+	if type(id) ~= "number" then
+		id = ("%q"):format(id)
+	end
+	return (codestring:gsub('"<threadId>"', id))
+end
 
 ---@class threadremote.ThreadRemote
 ---@operator call: threadremote.ThreadRemote
@@ -12,24 +29,61 @@ local ThreadRemote = class()
 ---@param t table
 function ThreadRemote:new(id, t)
 	self.id = id
-	self.remote_handler = RemoteHandler(t)
-	self.peer = LoveThreadPeer("thread_remote_input_" .. id)
+	self.t = t
+
+	self.input_channel = love.thread.getChannel("thread_remote_input_" .. id)
 	self.output_channel = love.thread.getChannel("thread_remote_output_" .. id)
+	self.input_channel:clear()
+	self.output_channel:clear()
+
+	self.remote_handler = RemoteHandler(t)
 	self.task_handler = TaskHandler(self.remote_handler)
-	self.remote = Remote(self.task_handler, self.peer)
+	self.remote = Remote(self.task_handler, self)
+
+	self.thread = love.thread.newThread(getCodeString(id))
+
+	function self.remote_handler.transform(_, th, peer, obj, ...)
+		local _obj = setmetatable({}, {__index = obj})
+		_obj.remote = Remote(th, peer)
+		return _obj, ...
+	end
+end
+
+---@param f fun(remote: table, ...: any): ...: any
+---@param ... any
+function ThreadRemote:start(f, ...)
+	self.thread:start()
+
+	self.input_channel:push({
+		name = "loadstring",
+		codestring = string.dump(f),
+		args = table_util.pack(...),
+	})
+end
+
+---@param msg icc.Message
+function ThreadRemote:send(msg)
+	self.input_channel:push({
+		name = "message",
+		msg = msg,
+	})
 end
 
 function ThreadRemote:update()
 	local task_handler = self.task_handler
 	local output_channel = self.output_channel
-	local msg = output_channel:pop()
-	while msg do
-		if msg.ret then
-			task_handler:handleReturn(msg)
-		else
-			task_handler:handleCall(self.peer, msg)
+	local event = output_channel:pop()
+	while event do
+		if event.name == "message" then
+			---@type icc.Message
+			local msg = setmetatable(event.msg, Message)
+			if msg.ret then
+				task_handler:handleReturn(msg)
+			else
+				task_handler:handleCall(self, msg)
+			end
 		end
-		msg = output_channel:pop()
+		event = output_channel:pop()
 	end
 end
 
--- threadremote/threadcode.lua
+++ threadremote/threadcode.lua
@@ -4,42 +4,58 @@ pkg.export_lua()
 
 local Remote = require("icc.Remote")
 local TaskHandler = require("icc.TaskHandler")
-
-local table_util = require("table_util")
+local RemoteHandler = require("icc.RemoteHandler")
+local Message = require("icc.Message")
 
 local threadId = "<threadId>"
 
-local inputChannel = love.thread.getChannel("input" .. threadId)
-local outputChannel = love.thread.getChannel("output" .. threadId)
+local input_channel = love.thread.getChannel("thread_remote_input_" .. threadId)
+local output_channel = love.thread.getChannel("thread_remote_output_" .. threadId)
+
+---@param _ any
+---@param msg icc.Message
+local peer = {send = function(_, msg)
+	output_channel:push({
+		name = "message",
+		msg = msg,
+	})
+end}
+
+local remote_handler = RemoteHandler({})
+local task_handler = TaskHandler(remote_handler)
+local remote = Remote(task_handler, peer)
+
+function remote_handler.transform(_, th, peer, obj, ...)
+	local _obj = setmetatable({}, {__index = obj})
+	_obj.remote = Remote(th, peer)
+	return _obj, ...
+end
 
 require("love.timer")
-
-local function load_code_string(event)
-	return xpcall(
-		assert(loadstring(event.codestring)),
-		debug.traceback,
-		unpack(event.args, 1, event.args.n)
-	)
-end
 
 local function handle(event)
 	if event.name == "stop" then
 		return
 	elseif event.name == "loadstring" then
-		local result = table_util.pack(load_code_string(event))
-		result.name = "result"
-		outputChannel:push(result)
+		local f = assert(loadstring(event.codestring))
+		remote_handler.t = f(remote, unpack(event.args, 1, event.args.n))
+	elseif event.name == "message" then
+		---@type icc.Message
+		local msg = setmetatable(event.msg, Message)
+		if msg.ret then
+			task_handler:handleReturn(msg)
+		else
+			task_handler:handleCall(peer, msg)
+		end
 	else
 		error("unknown event " .. require("inspect")(event))
 	end
 end
 
 while true do
-	local event = inputChannel:pop()
+	local event = input_channel:demand()
 
 	if event then
 		handle(event)
-	else
-		love.timer.sleep(0.001)
 	end
 end
