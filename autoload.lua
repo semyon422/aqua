2abaeb7f6a865b4de5e4f880f92bcb35bc49cd24
semyon422 <semyon422@users.noreply.github.com>
2025-04-04 13:57:14 +0500

    Fix autoload require errors in safe mod

--- autoload.lua
+++ autoload.lua
@@ -7,11 +7,15 @@ local mt = {}
 ---@return any?
 function mt.__index(t, k)
 	local prefix, safe = t[1], t[2]
+	local mod_name = prefix .. "." .. k
 	---@type boolean, any
-	local ok, mod = pcall(require, prefix .. "." .. k)
+	local ok, mod = pcall(require, mod_name)
 	if not safe then
 		assert(ok, mod)
 	elseif not ok then
+		if not mod:match(("module '%s' not found"):format(mod_name)) then
+			error(mod)
+		end
 		mod = true -- behave like empty file
 	end
 	t[k] = mod
