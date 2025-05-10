286771753240494ed1577ebf9320ab85654a85e8
semyon422 <semyon422@users.noreply.github.com>
2025-05-10 20:13:40 +0500

    Add io_util.write_file

--- io_util.lua
+++ io_util.lua
@@ -19,4 +19,25 @@ function io_util.read_file(path)
 	return assert(io_util.read_file_safe(path))
 end
 
+---@param path string
+---@param data string
+---@return true?
+---@return string?
+function io_util.write_file_safe(path, data)
+	local f, err = io.open(path, "wb")
+	if not f then
+		return nil, err
+	end
+	f:write(data)
+	f:close()
+	return true
+end
+
+---@param path string
+---@param data string
+---@return true
+function io_util.write_file(path, data)
+	return assert(io_util.write_file_safe(path, data))
+end
+
 return io_util
