50281184efa6ee3a276e7ceee82e5310071ade0d
semyon422 <semyon422@users.noreply.github.com>
2025-05-17 20:15:52 +0500

    Compress message in StringBufferPeer

--- icc/StringBufferPeer.lua
+++ icc/StringBufferPeer.lua
@@ -1,4 +1,5 @@
 local buffer = require("string.buffer")
+local zlib = require("zlib")
 local IPeer = require("icc.IPeer")
 local Message = require("icc.Message")
 
@@ -9,16 +10,22 @@ local StringBufferPeer = IPeer + {}
 ---@param msg icc.Message
 ---@return string
 function StringBufferPeer:encode(msg)
-	return buffer.encode(msg)
+	return zlib.deflate(buffer.encode(msg))
 end
 
 ---@param s string
 ---@return icc.Message?
 function StringBufferPeer:decode(s)
-	local ok, msg = pcall(buffer.decode, s)
+	local ok, data = pcall(zlib.inflate, s)
 	if not ok then
 		return
 	end
+
+	local ok, msg = pcall(buffer.decode, data)
+	if not ok then
+		return
+	end
+
 	---@cast msg table
 	return setmetatable(msg, Message)
 end
