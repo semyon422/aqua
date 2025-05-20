da5357b8d21aa361bbc15ba093333d3553590a56
semyon422 <semyon422@users.noreply.github.com>
2025-05-20 17:28:34 +0500

    Improve error messages and impl whitelist for RemoteHandler

--- icc/RemoteHandler.lua
+++ icc/RemoteHandler.lua
@@ -1,13 +1,18 @@
 local IHandler = require("icc.IHandler")
 local Remote = require("icc.Remote")
 
+---@class icc.RemoteHandlerWhitelist
+---@field [string] true|icc.RemoteHandlerWhitelist?
+
 ---@class icc.RemoteHandler: icc.IHandler
 ---@operator call: icc.RemoteHandler
 local RemoteHandler = IHandler + {}
 
 ---@param t {[any]: [any]}
-function RemoteHandler:new(t)
+---@param whitelist icc.RemoteHandlerWhitelist?
+function RemoteHandler:new(t, whitelist)
 	self.t = t
+	self.whitelist = whitelist
 end
 
 ---@param th icc.TaskHandler
@@ -26,14 +31,34 @@ end
 ---@param ... any
 ---@return any ...
 function RemoteHandler:handle(th, peer, path, is_method, ...)
+	local whitelist = self.whitelist
+
 	---@type any
-	local s, t = nil, self.t
+	local _self, value, prev_key = nil, self.t, nil
 	for _, k in ipairs(path) do
-		s, t = t, t[k]
+		local _type = type(value)
+		if _type ~= "table" then
+			error(("attempt to index field '%s' (a %s value)"):format(prev_key, _type))
+		end
+
+		if whitelist then
+			local _wl = whitelist[k]
+			if not _wl then
+				error(("attempt to get field '%s' (not whitelisted)"):format(k))
+			end
+			whitelist = _wl
+		end
+
+		_self, value, prev_key = value, value[k], k
 	end
 
-	---@cast t -table, +function
-	return t(select(is_method and 1 or 2, self:transform(th, peer, s, ...)))
+	local _type = type(value)
+	if _type ~= "function" then
+		error(("attempt to call field '%s' (a %s value)"):format(prev_key, _type))
+	end
+
+	---@cast value -table, +function
+	return value(select(is_method and 1 or 2, self:transform(th, peer, _self, ...)))
 end
 
 return RemoteHandler
