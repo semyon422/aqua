93b3e2003aa816f8619e7df9f99f77aed0800ec9
semyon422 <semyon422@users.noreply.github.com>
2025-08-12 21:45:44 +0500

    Add fs module with Love/FakeFilesystem: IFilesystem

---
+++ fs/LoveFilesystem.lua
@@ -0,0 +1,60 @@
+local IFilesystem = require("fs.IFilesystem")
+
+---@class fs.LoveFilesystem: fs.IFilesystem
+---@operator call: fs.LoveFilesystem
+local LoveFilesystem = IFilesystem + {}
+
+---@param path string
+---@param info? table
+---@return fs.FileInfo?
+function LoveFilesystem:getInfo(path, info)
+	return love.filesystem.getInfo(path, info)
+end
+
+---@param path string
+---@return boolean
+function LoveFilesystem:createDirectory(path)
+	return love.filesystem.createDirectory(path)
+end
+
+---@param dir string
+---@return table
+function LoveFilesystem:getDirectoryItems(dir)
+	return love.filesystem.getDirectoryItems(dir)
+end
+
+---@param name string
+---@param size? number
+---@return string?
+---@return string?
+function LoveFilesystem:read(name, size)
+	local content, err = love.filesystem.read(name, size)
+	if not content then
+		---@cast err -number, +string
+		return nil, err
+	end
+	return content
+end
+
+---@param name string
+---@param data string
+---@param size? number
+---@return boolean
+---@return string
+function LoveFilesystem:write(name, data, size)
+	return love.filesystem.write(name, data, size)
+end
+
+---@param name string
+---@return boolean
+function LoveFilesystem:remove(name)
+	return love.filesystem.remove(name)
+end
+
+---@param name string
+---@return boolean
+function LoveFilesystem:createDirectory(name)
+	return love.filesystem.createDirectory(name)
+end
+
+return LoveFilesystem
