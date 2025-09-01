675ed6b8e48f5961edbc5e06d3c2d4c3486f09f9
Nimue <83221369+Nimue-lua@users.noreply.github.com>
2025-09-01 18:21:23 +0300

    Update ScrollArea

--- ui/simple/ScrollArea.lua
+++ ui/simple/ScrollArea.lua
@@ -2,119 +2,102 @@ local ui = require("ui")
 local math_util = require("math_util")
 
 ---@class ui.Simple.ScrollArea.Params
----@field stiffness number?
----@field damping number?
+---@field friction number?
 ---@field threshold number?
----@field distance number?
 ---@field max_scroll number
 
 ---@class ui.Simple.ScrollArea : ui.Drawable, ui.Simple.ScrollArea.Params
----@overload fun(params: ui.Simple.ScrollArea.Params
+---@overload fun(params: ui.Simple.ScrollArea.Params)
 local ScrollArea = ui.Drawable + {}
 
-local initial_stiffiness = 0.5
-local initial_damping = 0.15
+local frame_aim = 60
 
 function ScrollArea:load()
-	self.stiffness = initial_stiffiness
-	self.damping = initial_damping
-	self.threshold = 0.5
-	self.distance = 140
+	self.friction = self.friction or 0.95
+	self.threshold = self.threshold or 0.5
 	self.velocity = 0
-	self.target_y = 0
-	self.position = 0
+	self.scroll_position = 0
 	self.accepts_input = true
 
-	self.origin_scroll_position = 0
-	self.origin_drag_position = 0
-	self.drag = false
-end
-
----@param y number
-function ScrollArea:scrollTo(y)
-	self.stiffness = initial_stiffiness
-	self.damping = initial_damping
-	self:setTargetY(y)
+	self.drag_start_y = 0
+	self.drag_start_scroll_position = 0
+	self.last_position = 0
+	self.is_dragging = false
 end
 
 ---@return number
 function ScrollArea:getScrollPosition()
-	return self.position
+	return self.scroll_position
 end
 
 ---@param position number
----@private
 function ScrollArea:setScrollPosition(position)
-	self.position = math_util.clamp(position, 0, self.max_scroll)
+	self.scroll_position = math_util.clamp(position, 0, self.max_scroll)
 end
 
----@param y number
----@private
-function ScrollArea:setTargetY(y)
-	self.target_y = math_util.clamp(y, 0, self.max_scroll)
-end
-
----@param e ui.ScrollEvent
-function ScrollArea:onScroll(e)
-	self:setTargetY(self.position + e.direction_y * -self.distance)
-	self.stiffness = initial_stiffiness
-	self.damping = initial_damping
+---@param position number
+function ScrollArea:scrollTo(position)
+	self.target_scroll_position = math_util.clamp(position, 0, self.max_scroll)
+	self.velocity = 0
 end
 
 ---@param e ui.DragStartEvent
 function ScrollArea:onDragStart(e)
-	self.origin_scroll_position = self.position
-	self.origin_drag_position = e.y
-	self.stiffness = 0
-	self.damping = 0.9
-	self.drag = true
-end
-
----@param e ui.DragStartEvent
-function ScrollArea:onDragEnd(e)
-	self.drag = false
+	self.drag_start_y = e.y
+	self.drag_start_scroll_position = self.scroll_position
+	self.last_position = self.scroll_position
+	self.velocity = 0
+	self.is_dragging = true
+	self.target_scroll_position = nil
 end
 
 ---@param e ui.DragEvent
 function ScrollArea:onDrag(e)
-	local distance = (e.y - self.origin_drag_position) * (768 / love.graphics.getHeight())
-	self:setScrollPosition(self.origin_scroll_position - distance)
-	self:setTargetY(self.position)
+	local distance = (e.y - self.drag_start_y) * (768 / love.graphics.getHeight())
+	local new_position = self.drag_start_scroll_position - distance
+	self:setScrollPosition(new_position)
 end
 
-local frame_aim = 60
+---@param e ui.DragEndEvent
+function ScrollArea:onDragEnd(e)
+	self.is_dragging = false
+end
 
 function ScrollArea:update(dt)
 	dt = dt * frame_aim
 
-	if self.drag then
-		local dp = self.position - (self.last_drag_position or self.position)
-		self.velocity = dp / dt
-		self.last_drag_position = self.position
-		return
-	end
+	if self.is_dragging then
+		local position_delta = self.scroll_position - self.last_position
+		self.velocity = position_delta / dt
+		self.last_position = self.scroll_position
+	elseif self.target_scroll_position then
+		local diff = self.target_scroll_position - self.scroll_position
+		if math.abs(diff) > 0.5 then
+			self.scroll_position = self.scroll_position + diff * math.min(1, dt * 0.2)
+		else
+			self.scroll_position = self.target_scroll_position
+			self.target_scroll_position = nil
+		end
+	else
+		self.velocity = self.velocity * math.pow(self.friction, dt)
 
-	local position = self.position
-	local displacement = self.target_y - position
-	local acceleration = displacement * self.stiffness
-
-	self.velocity = self.velocity + acceleration * dt
-	self.velocity = self.velocity * math.pow(self.damping, dt)
-	position = position + self.velocity * dt
-
-	if math.abs(displacement) > self.threshold or math.abs(self.velocity) > 0.1 then
-		self:setScrollPosition(position)
+		if math.abs(self.velocity) > 0.1 then
+			local new_position = self.scroll_position + self.velocity * dt
+			self:setScrollPosition(new_position)
+		else
+			self.velocity = 0
+		end
 	end
 end
 
 ---@param ctx ui.TraversalContext
 function ScrollArea:updateChildren(ctx)
-	love.graphics.translate(0, self.position)
+	love.graphics.translate(0, self.scroll_position)
 	ui.Drawable.updateChildren(self, ctx)
 end
 
 function ScrollArea:drawChildren()
-	love.graphics.translate(0, self.position)
+	love.graphics.translate(0, self.scroll_position)
 	ui.Drawable.drawChildren(self)
 end
 
