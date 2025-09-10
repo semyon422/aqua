7efac7d95d6ee90348bd518288fcb14e4b67395d
Nimue <83221369+Nimue-lua@users.noreply.github.com>
2025-09-10 10:32:46 +0300

    Fix slider

--- ui/simple/Slider.lua
+++ ui/simple/Slider.lua
@@ -18,50 +18,46 @@ Slider.ClassName = "Slider"
 Slider.height = 24
 Slider.handles_mouse_input = true
 
-local thumb_padding = 6
-
 function Slider:load()
 	self.value = self.getValue()
-	self.thumb_position = self:thumbPositionFromPercent()
+	self.thumb_position = self:thumbPositionFromValue(self.value)
 	self.thumb_target_position = self.thumb_position
 end
 
-function Slider:percentFromValue(v)
-	return (v - self.min) / (self.max - self.min)
+---@param v number
+function Slider:thumbPositionFromValue(v)
+	local p = (v - self.min) / (self.max - self.min)
+	p = math_util.clamp(p, 0, 1)
+	return self.width * p
 end
 
-function Slider:thumbPositionFromPercent(p)
-	return self.thumb_max_width * p
-end
-
-function Slider:updateWorldTransform()
-	Drawable.updateWorldTransform(self)
-	local w, h = self:getDimensions()
-	self.thumb_max_width = w - thumb_padding
-	self.thumb_size = h - thumb_padding
+---@param x number
+function Slider:valueFromThumbPosition(x)
+	local p = math_util.clamp(x / self:getWidth(), 0, 1)
+	return ((self.max - self.min) * p) + self.min
 end
 
 ---@param e ui.DragEvent
 function Slider:onDrag(e)
-	self:calcTargetPercent(e.x)
+	self:calcTargetPercent(e.x, e.y)
 end
 
 ---@param e ui.MouseClickEvent
 function Slider:onMouseClick(e)
-	self:calcTargetPercent(e.x)
+	self:calcTargetPercent(e.x, e.y)
 end
 
 ---@param x number
-function Slider:calcTargetPercent(screen_mouse_x)
-	local x, y = self.world_transform:inverseTransformPoint(screen_mouse_x, 0)
-	self.thumb_target_position = self:thumbPositionFromPercent(p)
-	local p = math_util.round(p * (self.max - self.min) + self.min, self.step)
-	self.setValue(math_util.round(self.value, self.step))
+function Slider:calcTargetPercent(screen_mouse_x, screen_mouse_y)
+	local x, _ = self.world_transform:inverseTransformPoint(screen_mouse_x, screen_mouse_y)
+	local value = self:valueFromThumbPosition(x)
+	self.setValue(math_util.round(value, self.step))
+	self.thumb_target_position = self:thumbPositionFromValue(value)
 end
 
 function Slider:update(dt)
-	local diff = (self.target_percent - self.percent) * math.pow(0.65, dt / 0.016)
-	self.percent = self.target_percent - diff
+	local diff = (self.thumb_target_position - self.thumb_position) * math.pow(0.65, dt / 0.016)
+	self.thumb_position = self.thumb_target_position - diff
 end
 
 function Slider:draw()
@@ -69,12 +65,8 @@ function Slider:draw()
 	love.graphics.setColor(0.1, 0.1, 0.1, 1)
 	ui.rectangle(w, h, h / 2 + 1)
 
-	love.graphics.translate(thumb_padding / 2, thumb_padding / 2)
 	love.graphics.setColor(0.9, 0.9, 0.9, 1)
-
-	local thumb_max_width = self.thumb_max_width
-	self.thumb_width = math_util.clamp(thumb_max_width * self.percent, 0, thumb_max_width)
-	ui.rectangle(self.thumb_width, self.thumb_size, self.thumb_size / 2 + 1)
+	ui.rectangle(self.thumb_position, h, h / 2 + 1)
 end
 
 return Slider
