72f53453ac885b6394a059b845c87e25b9cad374
semyon422 <semyon422@users.noreply.github.com>
2026-01-29 13:31:55 +0500

    Colorize diffs of failed tests output

--- testing/Test.lua
+++ testing/Test.lua
@@ -30,6 +30,8 @@ function Test:assert(cond, err, ...)
 	return cond
 end
 
+---@param v any
+---@return any
 local function format_got_expected(v)
 	if type(v) ~= "table" then
 		return v
@@ -37,6 +39,9 @@ local function format_got_expected(v)
 	return stbl.encode(v, nil, true)
 end
 
+---@param a string
+---@param b string
+---@return string
 local function string_diff(a, b)
 	local out = {}
 	if #a ~= #b then
@@ -53,6 +58,56 @@ local function string_diff(a, b)
 	return table.concat(out, ", ")
 end
 
+---@param expected string
+---@param got string
+---@return string
+---@return string
+local function get_dual_diff(expected, got)
+	local len1, len2 = #expected, #got
+	---@type integer[][]
+	local matrix = {}
+
+	for i = 0, len1 do
+		matrix[i] = {}
+		for j = 0, len2 do matrix[i][j] = 0 end
+	end
+
+	for i = 1, len1 do
+		for j = 1, len2 do
+			if expected:sub(i, i) == got:sub(j, j) then
+				matrix[i][j] = matrix[i - 1][j - 1] + 1
+			else
+				matrix[i][j] = math.max(matrix[i - 1][j], matrix[i][j - 1])
+			end
+		end
+	end
+
+	local res_expected = {}
+	local res_got = {}
+	local i, j = len1, len2
+
+	local RESET = "\27[0m"
+	local RED = "\27[31m"
+	local GREEN = "\27[32m"
+
+	while i > 0 or j > 0 do
+		if i > 0 and j > 0 and expected:sub(i, i) == got:sub(j, j) then
+			local char = expected:sub(i, i)
+			table.insert(res_expected, 1, char)
+			table.insert(res_got, 1, char)
+			i, j = i - 1, j - 1
+		elseif j > 0 and (i == 0 or matrix[i][j - 1] >= matrix[i - 1][j]) then
+			table.insert(res_got, 1, GREEN .. got:sub(j, j) .. RESET)
+			j = j - 1
+		else
+			table.insert(res_expected, 1, RED .. expected:sub(i, i) .. RESET)
+			i = i - 1
+		end
+	end
+
+	return table.concat(res_expected), table.concat(res_got)
+end
+
 ---@param cond any?
 ---@param got any?
 ---@param expected any?
@@ -67,15 +122,18 @@ function Test:expected_assert(cond, got, expected, level)
 	got = format_got_expected(got)
 	expected = format_got_expected(expected)
 
+	local colored_expected, colored_got = get_dual_diff(tostring(expected), tostring(got))
+
+	---@type string[]
 	local out = {}
 	table.insert(out, ("%s:%s:"):format(line.short_src, line.currentline))
 	if self.name then
 		out[1] = ("%s (%s)"):format(out[1], self.name)
 	end
 	table.insert(out, "---- expected")
-	table.insert(out, tostring(expected))
+	table.insert(out, colored_expected)
 	table.insert(out, "---- got")
-	table.insert(out, tostring(got))
+	table.insert(out, colored_got)
 	table.insert(out, "---- end")
 
 	local tg, te = type(got), type(expected)
