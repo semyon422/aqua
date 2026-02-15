b84e1d72b2ce5919fa62ae1176da1b31da0666c8
Nimue <83221369+Nimue-lua@users.noreply.github.com>
2026-02-15 08:57:20 +0300

    Add Text class

--- ui/view/Label.lua
+++ ui/view/Label.lua
@@ -1,65 +1,37 @@
 local Node = require("ui.view.Node")
+local Text = require("ui.view.Text")
 
 ---@class view.Label : view.Node
----@operator call: view.Label
----@field font love.Font
----@field font_size number
----@field text string
+---@overload fun(text: view.Text): view.Label
+---@field label view.Text
 local Label = Node + {}
 
-function Label:new()
+---@param font love.Font
+---@param font_size number
+---@param text string
+function Label:new(font, font_size, text)
 	Node.new(self)
-	self.content_dirty = false
-end
-
----@param v love.Font
-function Label:setFont(v)
-	if self.font == v then
-		return
-	end
-	self.font = v
-	self:updateTextBatch()
+	self.text_obj = Text(font, font_size, text)
+	self.layout_box:setDimensions(self.text_obj.width, self.text_obj.height)
 end
 
 ---@param v number
 function Label:setFontSize(v)
-	if self.font_size == v then
-		return
-	end
-	self.font_size = v
-	self:updateTextBatch()
+	self.text_obj:setFontSize(v)
+	self.layout_box:setDimensions(self.text_obj.width, self.text_obj.height)
 end
 
 ---@param v string
 function Label:setText(v)
-	if self.text == v then
-		return
-	end
-	self.text = v
-	self:updateTextBatch()
-end
-
-function Label:updateTextBatch()
-	if not self.font or not self.text or not self.font_size then
-		return
-	end
-
-	if not self.text_batch then
-		self.text_batch = love.graphics.newText(self.font)
-	end
-
-	self.text_batch:set(self.text)
-	local w, h = self.text_batch:getDimensions()
-	self.scale = self.font_size / self.font:getHeight()
-	self.layout_box:setDimensions(w * self.scale, h * self.scale)
+	self.text_obj:setText(v)
+	self.layout_box:setDimensions(self.text_obj.width, self.text_obj.height)
 end
 
 function Label:draw()
-	love.graphics.draw(self.text_batch, 0, 0, 0, self.scale, self.scale)
+	self.text_obj:draw()
 end
 
 Label.Setters = setmetatable({
-	font = Label.setFont,
 	font_size = Label.setFontSize,
 	text = Label.setText
 }, { __index = Node.Setters })
