d6c71ce92279a15c3026d04fa71f1213f3552cd7
Nimue <83221369+Nimue-lua@users.noreply.github.com>
2026-06-04 10:31:28 +0300

    Move ui to rizu repo

--- ui/composition/Strategies.lua
+++
@@ -1,9 +0,0 @@
----@class ui.composition.Strategies
-local Strategies = {
-	Stack = require("ui.composition.Stack"),
-	Column = require("ui.composition.Column"),
-	Track = require("ui.composition.Track"),
-	Anchor = require("ui.composition.Anchor")
-}
-
-return Strategies
--- ui/input/events/KeyboardEvent.lua
+++
@@ -1,9 +0,0 @@
-local UIEvent = require("ui.input.UIEvent")
-
----@class ui.KeyboardEvent : ui.UIEvent
----@operator call: ui.KeyboardEvent
----@field key love.KeyConstant
----@field is_repeated boolean
-local KeyboardEvent = UIEvent + {}
-
-return KeyboardEvent
